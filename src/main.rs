1fcffe72a532f1d083ba66d34cf59afc721941ec
David Venhoek <david@tweedegolf.com>
2022-01-27 12:02:18 +0100

    Refactored handling of synchronization messages.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,155 @@ use ptp::{
     time::{OffsetTime, TimeType},
 };
 
+#[derive(Debug, Clone, Default)]
+struct IdSequencer {
+    cur_id: u16,
+}
+
+impl IdSequencer {
+    pub fn get(&mut self) -> u16 {
+        let result = self.cur_id;
+        self.cur_id += 1;
+        result
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+struct SyncState {
+    sync_id: Option<u16>,
+    delay_id: Option<u16>,
+    sync_correction: Option<OffsetTime>,
+    sync_send_time: Option<OffsetTime>,
+    sync_recv_time: Option<OffsetTime>,
+    delay_send_time: Option<OffsetTime>,
+    delay_recv_time: Option<OffsetTime>,
+}
+
+#[derive(Debug, Clone)]
+struct TimeMeasurement {
+    offset_master: OffsetTime,
+    mean_delay: OffsetTime,
+}
+
+impl SyncState {
+    pub fn handle_sync(&mut self, message: Message, recv_time: OffsetTime, delay_id: u16) {
+        // TODO: Improve message datastructures to better be able to pass around messages of known type
+        let content = match message.content() {
+            MessageContent::Sync(content) => content,
+            _ => panic!("wrong message type"),
+        };
+
+        self.sync_id = Some(message.header().sequence_id());
+        self.delay_id = Some(delay_id);
+        self.sync_recv_time = Some(recv_time);
+        self.delay_send_time = None;
+        self.delay_recv_time = None;
+        if message.header().flag_field().two_step_flag {
+            self.sync_correction = Some(OffsetTime::from_interval(
+                &message.header().correction_field(),
+            ));
+            self.sync_send_time = None;
+        } else {
+            self.sync_correction = None;
+            self.sync_send_time = Some(
+                OffsetTime::from_timestamp(&content.origin_timestamp())
+                    + OffsetTime::from_interval(&message.header().correction_field()),
+            );
+        }
+    }
+
+    pub fn handle_followup(&mut self, message: Message) {
+        // TODO: Improve message datastructures to better be able to pass around messages of known type
+        let content = match message.content() {
+            MessageContent::FollowUp(content) => content,
+            _ => panic!("wrong message type"),
+        };
+
+        // Ignore messages not belonging to currently processing sync
+        let expected_seq_id = match self.sync_id {
+            Some(id) => id,
+            None => return,
+        };
+        if expected_seq_id != message.header().sequence_id() {
+            return;
+        }
+        let sync_correction = match self.sync_correction {
+            Some(correction) => correction,
+            None => return,
+        };
+
+        // Absorb into state
+        self.sync_correction = None;
+        self.sync_send_time = Some(
+            OffsetTime::from_timestamp(&content.precise_origin_timestamp())
+                + OffsetTime::from_interval(&message.header().correction_field())
+                + sync_correction,
+        );
+    }
+
+    pub fn handle_delayreq(&mut self, message: Message, recv_time: OffsetTime) {
+        // Ignore messages not belonging to currently processing sync
+        let expected_seq_id = match self.delay_id {
+            Some(id) => id,
+            None => return,
+        };
+        if expected_seq_id != message.header().sequence_id() {
+            return;
+        }
+
+        // Absorb into state
+        self.delay_send_time = Some(recv_time);
+    }
+
+    pub fn handle_delayresp(&mut self, message: Message) {
+        // TODO: Improve message datastructures to better be able to pass around messages of known type
+        let content = match message.content() {
+            MessageContent::DelayResp(content) => content,
+            _ => panic!("wrong message type"),
+        };
+
+        // Ignore messages not belonging to currently processing sync
+        let expected_seq_id = match self.delay_id {
+            Some(id) => id,
+            None => return,
+        };
+        if expected_seq_id != message.header().sequence_id() {
+            return;
+        }
+
+        // Absorb into state
+        self.delay_recv_time = Some(
+            OffsetTime::from_timestamp(&content.receive_timestamp())
+                - OffsetTime::from_interval(&message.header().correction_field()),
+        );
+    }
+
+    pub fn extract_measurement(&mut self) -> Option<TimeMeasurement> {
+        let sync_send_time = self.sync_send_time?;
+        let sync_recv_time = self.sync_recv_time?;
+        let delay_send_time = self.delay_send_time?;
+        let delay_recv_time = self.delay_recv_time?;
+
+        // Calculate measurement results
+        let mean_delay = (sync_recv_time - sync_send_time + delay_recv_time - delay_send_time) / 2;
+        let offset_master = sync_recv_time - sync_send_time - mean_delay;
+
+        // Reset state
+        self.sync_id = None;
+        self.delay_id = None;
+        self.sync_correction = None;
+        self.sync_recv_time = None;
+        self.sync_send_time = None;
+        self.delay_recv_time = None;
+        self.delay_send_time = None;
+
+        Some(TimeMeasurement {
+            mean_delay,
+            offset_master,
+        })
+    }
+}
+
 fn main() {
     let (tx, rx) = channel();
     let port319 = NetworkPort::new(319, tx.clone(), true);
@@ -17,73 +166,39 @@ fn main() {
 
     let clock_identity = get_clock_id().unwrap();
 
-    let mut delay_req_seq_id: u16 = 0;
-
-    let mut last_sync_correction: Option<OffsetTime> = None;
-    let mut last_sync_send_time: Option<OffsetTime> = None;
-    let mut last_sync_recv_time: Option<OffsetTime> = None;
-    let mut last_delayreq_send_time: Option<OffsetTime> = None;
+    let mut delay_sequencer = IdSequencer::default();
+    let mut sync_state = SyncState::default();
 
     loop {
         let packet = rx.recv().unwrap();
         let message = Message::deserialize(&packet.data).unwrap();
-        if let Some(ts) = packet.timestamp {
-            if let MessageContent::Sync(syncmessage) = message.content() {
-                last_sync_send_time = None;
-                last_sync_correction = None;
-                last_delayreq_send_time = None;
-                last_sync_recv_time = Some(ts);
-                if message.header().flag_field().two_step_flag {
-                    last_sync_correction = Some(OffsetTime::from_interval(
-                        &message.header().correction_field(),
-                    ));
-                } else {
-                    send_delay_request(clock_identity, delay_req_seq_id, &port319);
-                    delay_req_seq_id += 1;
-                    last_sync_send_time = Some(
-                        OffsetTime::from_timestamp(&syncmessage.origin_timestamp())
-                            + OffsetTime::from_interval(&message.header().correction_field()),
-                    );
-                }
-            } else if let MessageContent::DelayReq(_delayreq) = message.content() {
-                last_delayreq_send_time = Some(ts);
-            } else {
-                println!("Received {:?} from {} at {}", message, packet.addr, ts);
+        match message.content() {
+            MessageContent::Sync(_) => {
+                let delay_id = delay_sequencer.get();
+                send_delay_request(clock_identity, delay_id, &port319);
+                sync_state.handle_sync(message, packet.timestamp.unwrap(), delay_id);
             }
-        } else {
-            if let MessageContent::FollowUp(followup) = message.content() {
-                if let Some(sync_correction) = last_sync_correction {
-                    send_delay_request(clock_identity, delay_req_seq_id, &port319);
-                    delay_req_seq_id += 1;
-                    last_sync_correction = None;
-                    last_sync_send_time = Some(
-                        OffsetTime::from_timestamp(&followup.precise_origin_timestamp())
-                            + OffsetTime::from_interval(&message.header().correction_field())
-                            + sync_correction,
-                    );
-                } else {
-                    println!("Warning: Ignored followup, missing sync");
-                }
-            } else if let MessageContent::DelayResp(delayresp) = message.content() {
-                // TODO: Filter to only use our responses
-                if let Some(delay_send) = last_delayreq_send_time {
-                    if let Some(sync_send) = last_sync_send_time {
-                        if let Some(sync_recv) = last_sync_recv_time {
-                            let delay_recv =
-                                OffsetTime::from_timestamp(&delayresp.receive_timestamp())
-                                    - OffsetTime::from_interval(
-                                        &message.header().correction_field(),
-                                    );
-                            let mean_delay = (sync_recv - delay_send + delay_recv - sync_send) / 2;
-                            println!("Mean delay: {}", mean_delay);
-                            let offset_from_master = sync_recv - sync_send - mean_delay;
-                            println!("Master offset: {}", offset_from_master);
-                        }
-                    }
-                }
-            } else {
-                println!("Received {:?} from {}", message, packet.addr);
+            MessageContent::FollowUp(_) => {
+                sync_state.handle_followup(message);
             }
+            MessageContent::DelayReq(_) => {
+                sync_state.handle_delayreq(message, packet.timestamp.unwrap());
+            }
+            MessageContent::DelayResp(_) => {
+                sync_state.handle_delayresp(message);
+            }
+            _ => {
+                if let Some(ts) = packet.timestamp {
+                    println!("Received {:?} from {} at {}", message, packet.addr, ts);
+                } else {
+                    println!("Received {:?} from {}", message, packet.addr);
+                }
+            }
+        }
+
+        // See if we have a measurement
+        if let Some(measurement) = sync_state.extract_measurement() {
+            println!("Measurement {:?}", measurement);
         }
     }
 }
