1cd0d0f1a048ea31d7d022c353ea16e576e2460a
Dion Dokter <diondokter@gmail.com>
2022-03-15 12:13:40 +0100

    Revert "fmt"
    This reverts commit c794434b8dc1dbc482e95fe8d203bad5a58ed0b4.

--- src/filters/mod.rs
+++ src/filters/mod.rs
@@ -10,7 +10,7 @@ pub trait Filter {
     /// Put a new measurement in the filter.
     /// The filter can then do some processing and return what it thinks should be the offset and frequency multiplier
     /// that the clock should be adjusted with.
-    ///
+    /// 
     /// *Note*: The returned values aren't necessarily the 'real' offset from the master time. To prevent overshooting,
     /// oscillating, etc, the filter will apply some algorithms to prevent that.
     fn absorb(&mut self, m: Measurement) -> (OffsetTime, f64);
