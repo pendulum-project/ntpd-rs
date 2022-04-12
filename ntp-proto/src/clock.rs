a00228cdd1bd2c4c9322136d8569c76ab448a638
David Venhoek <david@tweedegolf.com>
2022-04-12 12:28:39 +0200

    Added comment explaining need for clock trait.

--- ntp-proto/src/clock.rs
+++ ntp-proto/src/clock.rs
@@ -1,6 +1,8 @@
 use crate::{NtpDuration, NtpTimestamp};
 
 /// Interface for a clock settable by the ntp implementation.
+/// This needs to be a trait as a single system can have multiple clocks
+/// which need different implementation for steering and/or now.
 pub trait NtpClock {
     type Error: std::error::Error;
 
