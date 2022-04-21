a5fdbba94ca74765b62f3feefa9edf86af81e0ea
Folkert <folkert@folkertdev.nl>
2022-04-21 13:19:14 +0200

    fix fuzz tests

--- ntp-proto/src/lib.rs
+++ ntp-proto/src/lib.rs
@@ -10,7 +10,7 @@ mod time_types;
 
 pub use clock::NtpClock;
 #[cfg(feature = "fuzz")]
-pub use filter::fuzz_find_interval;
+pub use clock_select::fuzz_find_interval;
 pub use identifiers::ReferenceId;
 pub use packet::NtpHeader;
 pub use time_types::{NtpDuration, NtpTimestamp};
