a08d2c55bb25d77744458fadab35dadfca9b306f
David Venhoek <david@tweedegolf.com>
2022-05-24 16:10:59 +0200

    Implemented basic support for sentry.

--- ntp-daemon/src/main.rs
+++ ntp-daemon/src/main.rs
@@ -3,9 +3,35 @@
 use ntp_proto::SystemConfig;
 use std::error::Error;
 
+#[cfg(feature = "sentry")]
+fn init_tracing() -> sentry::ClientInitGuard {
+    use tracing_subscriber::{prelude::*, EnvFilter};
+
+    let guard = sentry::init(sentry::ClientOptions {
+        // Set this a to lower value in production
+        traces_sample_rate: 1.0,
+        ..sentry::ClientOptions::default()
+    });
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer().with_filter(EnvFilter::from_default_env()))
+        .with(sentry_tracing::layer())
+        .init();
+
+    guard
+}
+
+#[cfg(not(feature = "sentry"))]
+fn init_tracing() {
+    tracing_subscriber::fmt::init();
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    tracing_subscriber::fmt::init();
+    // Sentry has a guard we need to keep alive,
+    // so store it. The compiler will optimize
+    // this away when not using sentry.
+    let _guard = init_tracing();
 
     let config = SystemConfig::default();
 
