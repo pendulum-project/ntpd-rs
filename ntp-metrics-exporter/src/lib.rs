e77b99d525bd7536a96519a941c7a8c353f9cf95
Ruben Nijveld <ruben@tweedegolf.com>
2022-11-23 20:31:28 +0100

    Initial metrics exporter implementation

--- ntp-metrics-exporter/src/lib.rs
+++ ntp-metrics-exporter/src/lib.rs
@@ -20,7 +20,7 @@ struct ServerLabels {
 }
 
 #[derive(Default)]
-pub(crate) struct Metrics {
+pub struct Metrics {
     system_poll_interval: Gauge<f64>,
     system_poll_interval_exp: Gauge<f64>,
     system_precision: Gauge<f64>,
@@ -43,7 +43,7 @@ pub(crate) struct Metrics {
 }
 
 impl Metrics {
-    pub(crate) fn fill(&self, data: &ObservableState) {
+    pub fn fill(&self, data: &ObservableState) {
         self.system_poll_interval.set(
             data.system
                 .time_snapshot
@@ -133,135 +133,135 @@ impl Metrics {
                 .set(server.stats.response_send_errors.get());
         }
     }
-}
 
-pub(crate) fn create_registry(metrics: &Metrics) -> Registry<Box<dyn SendSyncEncodeMetric>> {
-    let mut registry = <Registry>::with_prefix("ntp");
+    pub fn registry(&self) -> Registry<Box<dyn SendSyncEncodeMetric>> {
+        let mut registry = <Registry>::with_prefix("ntp");
 
-    let system = registry.sub_registry_with_prefix("system");
+        let system = registry.sub_registry_with_prefix("system");
 
-    system.register_with_unit(
-        "poll_interval",
-        "Time between polls of the system",
-        Unit::Seconds,
-        Box::new(metrics.system_poll_interval.clone()),
-    );
-    system.register(
-        "poll_interval",
-        "Exponent of time between poll intervals",
-        Box::new(metrics.system_poll_interval_exp.clone()),
-    );
-    system.register_with_unit(
-        "precision",
-        "Precision of the local clock",
-        Unit::Seconds,
-        Box::new(metrics.system_precision.clone()),
-    );
-    system.register_with_unit(
-        "accumulated_steps",
-        "Accumulated amount of seconds that the system needed to jump the time",
-        Unit::Seconds,
-        Box::new(metrics.system_accumulated_steps.clone()),
-    );
-    system.register_with_unit(
-        "accumulated_steps_threshold",
-        "Threshold for the accumulated step amount at which the NTP daemon will exit (or -1 if no threshold was set)",
-        Unit::Seconds,
-        Box::new(metrics.system_accumulated_steps_threshold.clone()),
-    );
-    system.register(
-        "leap_indicator",
-        "Indicates that a leap second will take place",
-        Box::new(metrics.system_leap_indicator.clone()),
-    );
+        system.register_with_unit(
+            "poll_interval",
+            "Time between polls of the system",
+            Unit::Seconds,
+            Box::new(self.system_poll_interval.clone()),
+        );
+        system.register(
+            "poll_interval",
+            "Exponent of time between poll intervals",
+            Box::new(self.system_poll_interval_exp.clone()),
+        );
+        system.register_with_unit(
+            "precision",
+            "Precision of the local clock",
+            Unit::Seconds,
+            Box::new(self.system_precision.clone()),
+        );
+        system.register_with_unit(
+            "accumulated_steps",
+            "Accumulated amount of seconds that the system needed to jump the time",
+            Unit::Seconds,
+            Box::new(self.system_accumulated_steps.clone()),
+        );
+        system.register_with_unit(
+            "accumulated_steps_threshold",
+            "Threshold for the accumulated step amount at which the NTP daemon will exit (or -1 if no threshold was set)",
+            Unit::Seconds,
+            Box::new(self.system_accumulated_steps_threshold.clone()),
+        );
+        system.register(
+            "leap_indicator",
+            "Indicates that a leap second will take place",
+            Box::new(self.system_leap_indicator.clone()),
+        );
 
-    let peer = registry.sub_registry_with_prefix("peer");
+        let peer = registry.sub_registry_with_prefix("peer");
 
-    peer.register_with_unit(
-        "uptime",
-        "Time since the peer was started",
-        Unit::Seconds,
-        Box::new(metrics.peer_uptime.clone()),
-    );
+        peer.register_with_unit(
+            "uptime",
+            "Time since the peer was started",
+            Unit::Seconds,
+            Box::new(self.peer_uptime.clone()),
+        );
 
-    peer.register_with_unit(
-        "poll_interval",
-        "Time between polls of the peer",
-        Unit::Seconds,
-        Box::new(metrics.peer_poll_interval.clone()),
-    );
+        peer.register_with_unit(
+            "poll_interval",
+            "Time between polls of the peer",
+            Unit::Seconds,
+            Box::new(self.peer_poll_interval.clone()),
+        );
 
-    peer.register(
-        "poll_interval",
-        "Exponent of time between polls of the peer",
-        Box::new(metrics.peer_poll_interval_exp.clone()),
-    );
+        peer.register(
+            "poll_interval",
+            "Exponent of time between polls of the peer",
+            Box::new(self.peer_poll_interval_exp.clone()),
+        );
 
-    peer.register(
-        "reachability_status",
-        "Number of polls until the upstream server is unreachable, zero if it is",
-        Box::new(metrics.peer_reachability_status.clone()),
-    );
+        peer.register(
+            "reachability_status",
+            "Number of polls until the upstream server is unreachable, zero if it is",
+            Box::new(self.peer_reachability_status.clone()),
+        );
 
-    peer.register_with_unit(
-        "offset",
-        "Offset between the upstream server and system time",
-        Unit::Seconds,
-        Box::new(metrics.peer_offset.clone()),
-    );
+        peer.register_with_unit(
+            "offset",
+            "Offset between the upstream server and system time",
+            Unit::Seconds,
+            Box::new(self.peer_offset.clone()),
+        );
 
-    peer.register_with_unit(
-        "delay",
-        "Current round-trip delay to the upstream server",
-        Unit::Seconds,
-        Box::new(metrics.peer_delay.clone()),
-    );
+        peer.register_with_unit(
+            "delay",
+            "Current round-trip delay to the upstream server",
+            Unit::Seconds,
+            Box::new(self.peer_delay.clone()),
+        );
 
-    peer.register_with_unit(
-        "dispersion",
-        "Maximum error of the clock",
-        Unit::Seconds,
-        Box::new(metrics.peer_dispersion.clone()),
-    );
+        peer.register_with_unit(
+            "dispersion",
+            "Maximum error of the clock",
+            Unit::Seconds,
+            Box::new(self.peer_dispersion.clone()),
+        );
 
-    peer.register_with_unit(
-        "jitter",
-        "Variance of network latency",
-        Unit::Seconds,
-        Box::new(metrics.peer_jitter.clone()),
-    );
+        peer.register_with_unit(
+            "jitter",
+            "Variance of network latency",
+            Unit::Seconds,
+            Box::new(self.peer_jitter.clone()),
+        );
 
-    let server = registry.sub_registry_with_prefix("server");
+        let server = registry.sub_registry_with_prefix("server");
 
-    server.register(
-        "received_packets",
-        "Number of incoming received packets",
-        Box::new(metrics.server_received_packets.clone()),
-    );
+        server.register(
+            "received_packets",
+            "Number of incoming received packets",
+            Box::new(self.server_received_packets.clone()),
+        );
 
-    server.register(
-        "accepted_packets",
-        "Number of packets accepted",
-        Box::new(metrics.server_accepted_packets.clone()),
-    );
+        server.register(
+            "accepted_packets",
+            "Number of packets accepted",
+            Box::new(self.server_accepted_packets.clone()),
+        );
 
-    server.register(
-        "denied_packets",
-        "Number of denied packets",
-        Box::new(metrics.server_denied_packets.clone()),
-    );
+        server.register(
+            "denied_packets",
+            "Number of denied packets",
+            Box::new(self.server_denied_packets.clone()),
+        );
 
-    server.register(
-        "rate_limited_packets",
-        "Number of rate limited packets",
-        Box::new(metrics.server_rate_limited_packets.clone()),
-    );
+        server.register(
+            "rate_limited_packets",
+            "Number of rate limited packets",
+            Box::new(self.server_rate_limited_packets.clone()),
+        );
 
-    server.register(
-        "response_send_errors",
-        "Number of packets where there was an error responding",
-        Box::new(metrics.server_response_send_errors.clone()),
-    );
+        server.register(
+            "response_send_errors",
+            "Number of packets where there was an error responding",
+            Box::new(self.server_response_send_errors.clone()),
+        );
 
-    registry
+        registry
+    }
 }
