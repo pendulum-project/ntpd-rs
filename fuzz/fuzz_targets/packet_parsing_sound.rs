361ee8930261eb11f99ef20de996738b9b8f92a0
Folkert <folkert@folkertdev.nl>
2022-12-15 16:13:35 +0100

    make fuzz tests compile

--- fuzz/fuzz_targets/packet_parsing_sound.rs
+++ fuzz/fuzz_targets/packet_parsing_sound.rs
@@ -2,11 +2,15 @@
 use libfuzzer_sys::fuzz_target;
 use ntp_proto::NtpPacket;
 use pretty_assertions::assert_eq;
+use std::io::Cursor;
 
 fuzz_target!(|data: Vec<u8>| {
+    let mut buf = [0u8; 4096];
     if let Ok(a) = NtpPacket::deserialize_without_decryption(&data) {
-        let mut buf = vec![];
-        a.serialize_without_encryption(&mut buf).unwrap();
-        assert_eq!(data, buf);
+        let mut cursor = Cursor::new(buf.as_mut_slice());
+        a.serialize_without_encryption(&mut cursor).unwrap();
+        let used = cursor.position();
+        let slice = &buf[..used as usize];
+        assert_eq!(data, slice);
     }
 });
