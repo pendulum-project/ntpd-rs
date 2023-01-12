64c293b80d3f5e5d42be3c1749096ada495457e4
Folkert <folkert@folkertdev.nl>
2023-01-12 15:02:21 +0100

    fix fuzzer

--- fuzz/fuzz_targets/packet_parsing_sound.rs
+++ fuzz/fuzz_targets/packet_parsing_sound.rs
@@ -1,11 +1,17 @@
 #![no_main]
+
+use std::io::Cursor;
+
 use libfuzzer_sys::fuzz_target;
 use ntp_proto::NtpPacket;
 
 fuzz_target!(|data: Vec<u8>| {
-    if let Ok(a) = NtpPacket::deserialize(&data) {
-        let mut buf = vec![];
-        a.serialize(&mut buf).unwrap();
-        assert_eq!(data, buf);
+    let mut buf = [0u8; 4096];
+    if let Ok(a) = NtpPacket::deserialize(&data, None) {
+        let mut cursor = Cursor::new(buf.as_mut_slice());
+        a.serialize(&mut cursor, None).unwrap();
+        let used = cursor.position();
+        let slice = &buf[..used as usize];
+        assert_eq!(data, slice);
     }
 });
