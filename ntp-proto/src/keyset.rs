d691994c647a8da820df81a7ab3643a1bfdd463f
David Venhoek <david@tweedegolf.com>
2023-02-23 11:40:57 +0100

    Implemented keyset provider.

--- ntp-proto/src/keyset.rs
+++ ntp-proto/src/keyset.rs
@@ -1,4 +1,6 @@
-use aead::generic_array::GenericArray;
+use std::sync::Arc;
+
+use aead::{generic_array::GenericArray, KeyInit};
 
 use crate::{
     nts_record::AeadAlgorithm,
@@ -17,6 +19,59 @@ impl std::fmt::Debug for DecodedServerCookie {
         f.debug_struct("DecodedServerCookie")
             .field("algorithm", &self.algorithm)
             .finish()
+    }
+}
+
+#[derive(Debug)]
+pub struct KeySetProvider {
+    current: Arc<KeySet>,
+    history: usize,
+}
+
+impl KeySetProvider {
+    /// Create a new keysetprovider that keeps history old
+    /// keys around (so in total, history+1 keys are valid
+    /// at any time)
+    pub fn new(history: usize) -> Self {
+        KeySetProvider {
+            current: Arc::new(KeySet {
+                keys: vec![AesSivCmac512::new(aes_siv::Aes256SivAead::generate_key(
+                    rand::thread_rng(),
+                ))],
+                id_offset: 0,
+                primary: 0,
+            }),
+            history,
+        }
+    }
+
+    /// Rotate a new key in as primary, forgetting an old one if needed
+    pub fn rotate(&mut self) {
+        let next_key = AesSivCmac512::new(aes_siv::Aes256SivAead::generate_key(rand::thread_rng()));
+        let mut keys = Vec::with_capacity((self.history + 1).min(self.current.keys.len() + 1));
+        for key in self.current.keys
+            [self.current.keys.len().saturating_sub(self.history)..self.current.keys.len()]
+            .iter()
+        {
+            // This is the rare case where we do really want to make a copy.
+            keys.push(AesSivCmac512::new(GenericArray::clone_from_slice(
+                key.key_bytes(),
+            )));
+        }
+        keys.push(next_key);
+        self.current = Arc::new(KeySet {
+            id_offset: self
+                .current
+                .id_offset
+                .wrapping_add(self.current.keys.len().saturating_sub(self.history) as u32),
+            primary: keys.len() as u32,
+            keys,
+        })
+    }
+
+    /// Get the current KeySet
+    pub fn get(&self) -> Arc<KeySet> {
+        self.current.clone()
     }
 }
 
@@ -41,7 +96,7 @@ impl KeySet {
             .expect("Failed to encrypt cookie");
 
         let mut output = Vec::with_capacity(4 + encrypted.nonce.len() + encrypted.ciphertext.len());
-        output.extend((self.primary + self.id_offset).to_be_bytes());
+        output.extend((self.primary.wrapping_add(self.id_offset)).to_be_bytes());
         output.extend(encrypted.nonce);
         output.extend(encrypted.ciphertext);
         output
@@ -54,11 +109,10 @@ impl KeySet {
         }
 
         let id = u32::from_be_bytes(cookie[0..4].try_into().unwrap());
-
-        if id < self.id_offset || (id - self.id_offset) as usize >= self.keys.len() {
+        let id = id.wrapping_sub(self.id_offset) as usize;
+        if id >= self.keys.len() {
             return Err(DecryptError);
         }
-        let id = (id - self.id_offset) as usize;
 
         let plaintext = self.keys[id].decrypt(&cookie[4..20], &cookie[20..], &[])?;
 
@@ -172,5 +226,33 @@ mod tests {
         assert_eq!(decoded.algorithm, round.algorithm);
         assert_eq!(decoded.s2c.key_bytes(), round.s2c.key_bytes());
         assert_eq!(decoded.c2s.key_bytes(), round.c2s.key_bytes());
+    }
+
+    #[test]
+    fn old_cookie_still_valid() {
+        let decoded = DecodedServerCookie {
+            algorithm: AeadAlgorithm::AeadAesSivCmac256,
+            s2c: Box::new(AesSivCmac256::new((0..32_u8).collect())),
+            c2s: Box::new(AesSivCmac256::new((32..64_u8).collect())),
+        };
+
+        let mut provider = KeySetProvider::new(1);
+        let encoded = provider.get().encode_cookie(&decoded);
+
+        let round = provider.get().decode_cookie(&encoded).unwrap();
+        assert_eq!(decoded.algorithm, round.algorithm);
+        assert_eq!(decoded.s2c.key_bytes(), round.s2c.key_bytes());
+        assert_eq!(decoded.c2s.key_bytes(), round.c2s.key_bytes());
+
+        provider.rotate();
+
+        let round = provider.get().decode_cookie(&encoded).unwrap();
+        assert_eq!(decoded.algorithm, round.algorithm);
+        assert_eq!(decoded.s2c.key_bytes(), round.s2c.key_bytes());
+        assert_eq!(decoded.c2s.key_bytes(), round.c2s.key_bytes());
+
+        provider.rotate();
+
+        assert!(provider.get().decode_cookie(&encoded).is_err());
     }
 }
