9f4a64290ae24d0e2939a9ac18e90942ccca987c
Folkert <folkert@folkertdev.nl>
2023-03-17 22:15:38 +0100

    make the unix ntp clock more general to support hardware clocks

--- ntp-os-clock/src/unix.rs
+++ ntp-os-clock/src/unix.rs
@@ -5,10 +5,7 @@
 // is constructed in such a way that use of the public functions is
 // safe regardless of given arguments.
 
-use std::{
-    os::unix::io::{AsRawFd, RawFd},
-    path::Path,
-};
+use std::{ffi::CStr, os::fd::RawFd};
 
 use crate::{Error, EPOCH_OFFSET};
 use ntp_proto::{NtpClock, NtpDuration, NtpLeapIndicator, NtpTimestamp, PollInterval};
@@ -106,7 +103,7 @@ pub(crate) const EMPTY_TIMEX: libc::timex = libc::timex {
 /// current time.
 // Implementation note: this is intentionally a bare struct, the NTP Clock defined
 // in the NTP KAPI is unique and no state is needed to interact with it.
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone)]
 pub struct UnixNtpClock {
     clock: libc::clockid_t,
 }
@@ -120,59 +117,47 @@ impl UnixNtpClock {
         Self { clock: id }
     }
 
-    pub fn from_path(path: &Path) -> Result<Self, Error> {
-        match std::fs::File::options().read(true).write(true).open(path) {
-            Err(_) => Err(convert_errno()),
-            Ok(file) => {
-                let fd = file.as_raw_fd();
-
-                // never close the file, keep it open so clock steering can use the file descriptor
-                std::mem::forget(file);
-
-                Ok(Self::from_file_descriptor(fd))
-            }
-        }
+    #[cfg_attr(target_os = "linux", allow(unused))]
+    pub fn ptp0() -> Result<Self, Error> {
+        let path = CStr::from_bytes_with_nul(b"/dev/ptp0\0").unwrap();
+        // SAFETY
+        //
+        // we know this is a file descriptor of a clock
+        unsafe { Self::from_path(path) }
     }
 
-    pub fn from_file_descriptor(fd: RawFd) -> Self {
-        // using an invalid clock id is safe. The function that take this value as an argument will
-        // return an EINVAL IO error when the clock id is invalid.
+    unsafe fn from_path(path: &CStr) -> Result<Self, Error> {
+        let fd = match unsafe { libc::open(path.as_ptr(), libc::O_RDWR) } {
+            -1 => return Err(convert_errno()),
+            valid => valid,
+        };
 
+        Ok(unsafe { Self::from_file_descriptor(fd as RawFd) })
+    }
+
+    unsafe fn from_file_descriptor(fd: RawFd) -> Self {
         let id = ((!(fd as libc::clockid_t)) << 3) | 0b11;
+
         Self::custom(id)
     }
 
-    #[cfg_attr(target_os = "linux", allow(unused))]
     fn clock_gettime(&self) -> Result<libc::timespec, Error> {
         let mut timespec = libc::timespec {
             tv_sec: 0,
             tv_nsec: 0,
         };
 
-        // # Safety
-        //
-        // using an invalid clock id is safe. `clock_adjtime` will return an EINVAL error
-        // https://linux.die.net/man/3/clock_gettime
-        //
-        // The timespec pointer is valid.
         cerr(unsafe { libc::clock_gettime(self.clock, &mut timespec) })?;
 
         Ok(timespec)
     }
 
-    #[cfg_attr(target_os = "linux", allow(unused))]
     fn clock_settime(&self, mut timespec: libc::timespec) -> Result<(), Error> {
         while timespec.tv_nsec > 1_000_000_000 {
             timespec.tv_sec += 1;
             timespec.tv_nsec -= 1_000_000_000;
         }
 
-        // # Safety
-        //
-        // using an invalid clock id is safe. `clock_adjtime` will return an EINVAL error
-        // https://linux.die.net/man/3/clock_settime
-        //
-        // The timespec pointer is valid.
         unsafe { cerr(libc::clock_settime(self.clock, &timespec))? };
 
         Ok(())
@@ -180,28 +165,10 @@ impl UnixNtpClock {
 
     fn clock_adjtime(&self, timex: &mut libc::timex) -> Result<(), Error> {
         // We don't care about the time status, so the non-error
-        // information in the return value of clock_adjtime can be ignored.
-        //
-        // # Safety
-        //
-        // The clock_adjtime call is safe because the reference always
+        // information in the return value of ntp_adjtime can be ignored.
+        // The ntp_adjtime call is safe because the reference always
         // points to a valid libc::timex.
-        //
-        // using an invalid clock id is safe. `clock_adjtime` will return an EINVAL error
-        // https://man.archlinux.org/man/clock_adjtime.2.en#EINVAL~4
-        #[cfg(target_os = "linux")]
-        use libc::clock_adjtime as adjtime;
-
-        #[cfg(any(target_os = "freebsd", target_os = "macos"))]
-        let adjtime = {
-            extern "C" {
-                fn clock_adjtime(clk_id: libc::clockid_t, buf: *mut libc::timex) -> libc::c_int;
-            }
-
-            clock_adjtime
-        };
-
-        if unsafe { adjtime(self.clock, timex) } == -1 {
+        if unsafe { libc::clock_adjtime(self.clock, timex) } == -1 {
             Err(convert_errno())
         } else {
             Ok(())
@@ -209,20 +176,11 @@ impl UnixNtpClock {
     }
 
     fn ntp_adjtime(timex: &mut libc::timex) -> Result<(), Error> {
-        #[cfg(any(target_os = "freebsd", target_os = "macos", target_env = "gnu"))]
-        use libc::ntp_adjtime as adjtime;
-
-        // ntp_adjtime is equivalent to adjtimex for our purposes
-        //
-        // https://man7.org/linux/man-pages/man2/adjtimex.2.html
-        #[cfg(all(target_os = "linux", target_env = "musl"))]
-        use libc::adjtimex as adjtime;
-
         // We don't care about the time status, so the non-error
         // information in the return value of ntp_adjtime can be ignored.
         // The ntp_adjtime call is safe because the reference always
         // points to a valid libc::timex.
-        if unsafe { adjtime(timex) } == -1 {
+        if unsafe { libc::ntp_adjtime(timex) } == -1 {
             Err(convert_errno())
         } else {
             Ok(())
@@ -234,71 +192,6 @@ impl UnixNtpClock {
             Self::ntp_adjtime(timex)
         } else {
             self.clock_adjtime(timex)
-        }
-    }
-
-    #[cfg_attr(target_os = "linux", allow(unused))]
-    fn step_clock_timespec(&self, offset: ntp_proto::NtpDuration) -> Result<NtpTimestamp, Error> {
-        let (offset_secs, offset_nanos) = offset.as_seconds_nanos();
-
-        let mut timespec = self.clock_gettime()?;
-
-        timespec.tv_sec += {
-            // this looks a little strange. it is to work around the `libc::time_t` type being
-            // deprectated for musl in rust's libc.
-            if true {
-                offset_secs as _
-            } else {
-                timespec.tv_sec
-            }
-        };
-        timespec.tv_nsec += offset_nanos as libc::c_long;
-
-        self.clock_settime(timespec)?;
-
-        Ok(current_time_timespec(timespec, Precision::Nano))
-    }
-
-    #[cfg(target_os = "linux")]
-    fn step_clock_timex(&self, offset: ntp_proto::NtpDuration) -> Result<NtpTimestamp, Error> {
-        let (secs, nanos) = offset.as_seconds_nanos();
-
-        let mut timex = libc::timex {
-            modes: libc::ADJ_SETOFFSET | libc::MOD_NANO,
-            time: libc::timeval {
-                tv_sec: secs as _,
-                tv_usec: nanos as libc::suseconds_t,
-            },
-            ..crate::unix::EMPTY_TIMEX
-        };
-
-        self.adjtime(&mut timex)?;
-        self.extract_current_time(&timex)
-    }
-
-    fn extract_current_time(&self, _timex: &libc::timex) -> Result<NtpTimestamp, Error> {
-        #[cfg(target_os = "linux")]
-        {
-            // hardware clocks may not report the timestamp
-            if _timex.time.tv_sec != 0 && _timex.time.tv_usec != 0 {
-                // in a timex, the status flag determines precision
-                let precision = match _timex.status & libc::STA_NANO {
-                    0 => Precision::Micro,
-                    _ => Precision::Nano,
-                };
-
-                Ok(current_time_timeval(_timex.time, precision))
-            } else {
-                let timespec = self.clock_gettime()?;
-                Ok(current_time_timespec(timespec, Precision::Nano))
-            }
-        }
-
-        #[cfg(any(target_os = "freebsd", target_os = "macos"))]
-        {
-            // clock_gettime always gives nanoseconds
-            let timespec = self.clock_gettime()?;
-            Ok(current_time_timespec(timespec, Precision::Nano))
         }
     }
 }
@@ -393,6 +286,13 @@ fn ignore_not_supported(res: Result<(), Error>) -> Result<(), Error> {
         Err(Error::NotSupported) => Ok(()),
         other => other,
     }
+
+    #[cfg(not(target_os = "linux"))]
+    {
+        // clock_gettime always gives nanoseconds
+        let timespec = clock_gettime()?;
+        Ok(current_time_timespec(timespec, Precision::Nano))
+    }
 }
 
 impl NtpClock for UnixNtpClock {
@@ -412,21 +312,22 @@ impl NtpClock for UnixNtpClock {
         // NTP Kapi expects frequency adjustment in units of 2^-16 ppm
         // but our input is in units of seconds drift per second, so convert.
         ntp_kapi_timex.freq = (freq * 65536e6) as libc::c_long;
-        ntp_kapi_timex.status =
-            !libc::STA_PLL & !libc::STA_PPSFREQ & !libc::STA_FLL & !libc::STA_PPSTIME;
-
         self.adjtime(&mut ntp_kapi_timex)?;
-        self.extract_current_time(&ntp_kapi_timex)
+        extract_current_time(&ntp_kapi_timex)
     }
 
     #[cfg(target_os = "linux")]
     fn step_clock(&self, offset: ntp_proto::NtpDuration) -> Result<NtpTimestamp, Self::Error> {
-        self.step_clock_timex(offset)
-    }
+        let (offset_secs, offset_nanos) = offset.as_seconds_nanos();
 
-    #[cfg(any(target_os = "freebsd", target_os = "macos"))]
-    fn step_clock(&self, offset: ntp_proto::NtpDuration) -> Result<NtpTimestamp, Self::Error> {
-        self.step_clock_timespec(offset)
+        let mut timespec = self.clock_gettime()?;
+
+        timespec.tv_sec += offset_secs as libc::time_t;
+        timespec.tv_nsec += offset_nanos as libc::c_long;
+
+        self.clock_settime(timespec)?;
+
+        Ok(current_time_timespec(timespec, Precision::Nano))
     }
 
     fn enable_ntp_algorithm(&self) -> Result<(), Self::Error> {
@@ -450,9 +351,7 @@ impl NtpClock for UnixNtpClock {
 
         // Disable all kernel time control loops (phase lock, frequency lock, pps time and pps frequency).
         timex.status &= !libc::STA_PLL & !libc::STA_FLL & !libc::STA_PPSTIME & !libc::STA_PPSFREQ;
-
-        // ignore if we cannot disable the kernel time control loops (e.g. external clocks)
-        ignore_not_supported(self.adjtime(&mut timex))
+        self.adjtime(&mut timex)
     }
 
     fn ntp_algorithm_update(
@@ -464,7 +363,7 @@ impl NtpClock for UnixNtpClock {
         timex.modes = libc::MOD_OFFSET | libc::MOD_TIMECONST;
         timex.offset = duration_in_nanos(offset);
         timex.constant = poll_interval.as_log() as libc::c_long;
-        ignore_not_supported(self.adjtime(&mut timex))
+        self.adjtime(&mut timex)
     }
 
     fn error_estimate_update(
@@ -476,7 +375,7 @@ impl NtpClock for UnixNtpClock {
         timex.modes = libc::MOD_ESTERROR | libc::MOD_MAXERROR;
         timex.esterror = duration_in_nanos(est_error) / 1000;
         timex.maxerror = duration_in_nanos(max_error) / 1000;
-        ignore_not_supported(self.adjtime(&mut timex))
+        self.adjtime(&mut timex)
     }
 
     fn status_update(&self, leap_status: NtpLeapIndicator) -> Result<(), Self::Error> {
@@ -492,7 +391,7 @@ impl NtpClock for UnixNtpClock {
             NtpLeapIndicator::Leap59 => timex.status |= libc::STA_DEL,
             NtpLeapIndicator::Unknown => timex.status |= libc::STA_UNSYNC,
         }
-        ignore_not_supported(self.adjtime(&mut timex))
+        self.adjtime(&mut timex)
     }
 }
 
