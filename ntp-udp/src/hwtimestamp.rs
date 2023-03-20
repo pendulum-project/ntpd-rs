8ec2d7a0ee86a492d15b43a1c0412cd73b28e8ee
Folkert <folkert@folkertdev.nl>
2023-03-20 11:23:50 +0100

    refactor

--- ntp-udp/src/hwtimestamp.rs
+++ ntp-udp/src/hwtimestamp.rs
@@ -2,6 +2,18 @@ use std::os::fd::AsRawFd;
 
 use crate::{interface_name, raw_socket::cerr};
 
+pub fn driver_enable_hardware_timestamping(
+    udp_socket: &std::net::UdpSocket,
+) -> std::io::Result<()> {
+    let mut tstamp_config = hwtimestamp_config {
+        flags: 0,
+        tx_type: HWTSTAMP_TX_ON,
+        rx_filter: HWTSTAMP_FILTER_ALL,
+    };
+
+    set_hardware_timestamp(udp_socket, tstamp_config)
+}
+
 const HWTSTAMP_TX_ON: libc::c_int = 1;
 const HWTSTAMP_FILTER_ALL: libc::c_int = 1;
 
@@ -16,49 +28,87 @@ pub struct hwtimestamp_config {
     pub rx_filter: libc::c_int,
 }
 
-pub fn driver_enable_hardware_timestamping(
+fn set_hardware_timestamp(
     udp_socket: &std::net::UdpSocket,
+    mut config: hwtimestamp_config,
 ) -> std::io::Result<()> {
-    let mut tstamp_config = hwtimestamp_config {
-        flags: 0,
-        tx_type: HWTSTAMP_TX_ON,
-        rx_filter: HWTSTAMP_FILTER_ALL,
+    let mut ifreq: libc::ifreq = libc::ifreq {
+        ifr_name: socket_interface_name(udp_socket)?,
+        ifr_ifru: libc::__c_anonymous_ifr_ifru {
+            ifru_data: (&mut config as *mut _) as *mut libc::c_char,
+        },
     };
 
-    if let Some(ifr_name) = interface_name::interface_name(udp_socket.local_addr()?)? {
-        let mut ifreq: libc::ifreq = libc::ifreq {
-            ifr_name,
-            ifr_ifru: libc::__c_anonymous_ifr_ifru {
-                ifru_data: (&mut tstamp_config as *mut _) as *mut libc::c_char,
-            },
-        };
+    let fd = udp_socket.as_raw_fd();
+    cerr(unsafe { libc::ioctl(fd, SIOCSHWTSTAMP as libc::c_ulong, &mut ifreq) })?;
 
-        let fd = udp_socket.as_raw_fd();
-        cerr(unsafe { libc::ioctl(fd, SIOCSHWTSTAMP as libc::c_ulong, &mut ifreq) })?;
+    Ok(())
+}
 
-        let mut tstamp_config = hwtimestamp_config {
-            flags: 0,
-            tx_type: 0,
-            rx_filter: 0,
-        };
+fn get_hardware_timestamp(udp_socket: &std::net::UdpSocket) -> std::io::Result<hwtimestamp_config> {
+    let mut tstamp_config = hwtimestamp_config {
+        flags: 0,
+        tx_type: 0,
+        rx_filter: 0,
+    };
 
-        let mut ifreq: libc::ifreq = libc::ifreq {
-            ifr_name,
-            ifr_ifru: libc::__c_anonymous_ifr_ifru {
-                ifru_data: (&mut tstamp_config as *mut _) as *mut libc::c_char,
-            },
-        };
+    let mut ifreq: libc::ifreq = libc::ifreq {
+        ifr_name: socket_interface_name(udp_socket)?,
+        ifr_ifru: libc::__c_anonymous_ifr_ifru {
+            ifru_data: (&mut tstamp_config as *mut _) as *mut libc::c_char,
+        },
+    };
 
-        let fd = udp_socket.as_raw_fd();
-        cerr(unsafe { libc::ioctl(fd, SIOCGHWTSTAMP as libc::c_ulong, &mut ifreq) })?;
+    let fd = udp_socket.as_raw_fd();
+    cerr(unsafe { libc::ioctl(fd, SIOCGHWTSTAMP as libc::c_ulong, &mut ifreq) })?;
 
-        dbg!(tstamp_config);
+    Ok(tstamp_config)
+}
+
+fn socket_interface_name(udp_socket: &std::net::UdpSocket) -> std::io::Result<[i8; 16]> {
+    use std::io::{Error, ErrorKind};
+
+    match interface_name::interface_name(udp_socket.local_addr()?)? {
+        Some(ifr_name) => Ok(ifr_name),
+        None => Err(Error::new(ErrorKind::Other, "socket has no interface name")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_hwtimestamp() -> std::io::Result<()> {
+        let udp_socket = std::net::UdpSocket::bind(("0.0.0.0", 9000))?;
+        udp_socket.connect(("10.0.0.18", 9001))?;
+
+        let _ = get_hardware_timestamp(&udp_socket)?;
 
         Ok(())
-    } else {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "socket has no interface name",
-        ))
+    }
+
+    #[test]
+    #[ignore = "requires elevated permissions to run"]
+    fn get_set_hwtimestamp() -> std::io::Result<()> {
+        let udp_socket = std::net::UdpSocket::bind(("0.0.0.0", 9002))?;
+        udp_socket.connect(("10.0.0.18", 9003))?;
+
+        let old = get_hardware_timestamp(&udp_socket)?;
+
+        let mut custom = hwtimestamp_config {
+            flags: 0,
+            tx_type: HWTSTAMP_TX_ON,
+            rx_filter: HWTSTAMP_FILTER_ALL,
+        };
+
+        set_hardware_timestamp(&udp_socket, custom)?;
+        let new = get_hardware_timestamp(&udp_socket)?;
+
+        assert_eq!(new.flags, 0);
+        assert_eq!(new.tx_type, HWTSTAMP_TX_ON);
+        assert_eq!(new.rx_filter, HWTSTAMP_FILTER_ALL);
+
+        Ok(())
     }
 }
