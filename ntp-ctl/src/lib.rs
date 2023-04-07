cdd9758e6e201dfad61b5d2018abab0657446f39
Folkert <folkert@tweedegolf.com>
2023-04-07 15:03:36 +0200

    make socket permissions stricter (and add tests)

--- ntp-ctl/src/lib.rs
+++ ntp-ctl/src/lib.rs
@@ -62,11 +62,15 @@ pub async fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
         .unwrap_or_else(|| PathBuf::from("/run/ntpd-rs/configure"));
 
     let socket_path = match cli.command {
-        Command::Peers | Command::System | Command::Prometheus => &observation,
-        Command::Config(_) => &configuration,
+        Command::Peers | Command::System | Command::Prometheus => observation,
+        Command::Config(_) => configuration,
     };
 
-    let mut stream = match tokio::net::UnixStream::connect(socket_path).await {
+    Ok(run(cli.command, socket_path).await?)
+}
+
+async fn run(command: Command, socket_path: PathBuf) -> Result<ExitCode, std::io::Error> {
+    let mut stream = match tokio::net::UnixStream::connect(&socket_path).await {
         Ok(stream) => stream,
         Err(e) => {
             eprintln!("Could not open socket at {}: {}", socket_path.display(), e);
@@ -74,7 +78,7 @@ pub async fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
         }
     };
 
-    match cli.command {
+    match command {
         Command::Peers => {
             let mut msg = Vec::with_capacity(16 * 1024);
             match ntp_daemon::sockets::read_json::<ObservableState>(&mut stream, &mut msg).await {
@@ -108,8 +112,6 @@ pub async fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
             }
         }
         Command::Prometheus => {
-            let mut stream = tokio::net::UnixStream::connect(observation).await?;
-
             let mut msg = Vec::with_capacity(16 * 1024);
             match ntp_daemon::sockets::read_json::<ObservableState>(&mut stream, &mut msg).await {
                 Ok(output) => {
@@ -117,7 +119,13 @@ pub async fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
                     metrics.fill(&output);
                     let registry = metrics.registry();
                     let mut buf = String::new();
-                    prometheus_client::encoding::text::encode(&mut buf, &registry)?;
+
+                    if let Err(e) = prometheus_client::encoding::text::encode(&mut buf, &registry) {
+                        eprintln!("Failed to encode prometheus data: {e}");
+
+                        return Ok(ExitCode::FAILURE);
+                    }
+
                     println!("{buf}");
 
                     Ok(ExitCode::SUCCESS)
@@ -139,5 +147,162 @@ pub async fn main() -> Result<ExitCode, Box<dyn std::error::Error>> {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::os::unix::prelude::PermissionsExt;
+
+    use ntp_daemon::{
+        config::ObserveConfig,
+        sockets::{create_unix_socket, read_json, write_json},
+    };
+
+    use super::*;
+
+    async fn write_socket_helper(
+        command: Command,
+        socket_name: &str,
+    ) -> std::io::Result<Result<ExitCode, std::io::Error>> {
+        let config: ObserveConfig = Default::default();
+
+        // be careful with copying: tests run concurrently and should use a unique socket name!
+        let path = std::env::temp_dir().join(socket_name);
+        if path.exists() {
+            std::fs::remove_file(&path).unwrap();
+        }
+
+        let peers_listener = create_unix_socket(&path)?;
+
+        let permissions: std::fs::Permissions = PermissionsExt::from_mode(config.mode);
+        std::fs::set_permissions(&path, permissions)?;
+
+        let fut = super::run(command, path);
+        let handle = tokio::spawn(fut);
+
+        let value = ObservableState {
+            system: Default::default(),
+            peers: vec![],
+            servers: vec![],
+        };
+
+        let (mut stream, _addr) = peers_listener.accept().await?;
+        write_json(&mut stream, &value).await?;
+
+        let result = handle.await.unwrap();
+
+        Ok(result)
+    }
+
+    #[tokio::test]
+    async fn test_control_socket_peer() -> std::io::Result<()> {
+        // be careful with copying: tests run concurrently and should use a unique socket name!
+        let result = write_socket_helper(Command::Peers, "ntp-test-stream-6").await?;
+
+        assert_eq!(
+            format!("{:?}", result.unwrap()),
+            format!("{:?}", ExitCode::SUCCESS)
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_control_socket_system() -> std::io::Result<()> {
+        // be careful with copying: tests run concurrently and should use a unique socket name!
+        let result = write_socket_helper(Command::System, "ntp-test-stream-7").await?;
+
+        assert_eq!(
+            format!("{:?}", result.unwrap()),
+            format!("{:?}", ExitCode::SUCCESS)
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_control_socket_prometheus() -> std::io::Result<()> {
+        // be careful with copying: tests run concurrently and should use a unique socket name!
+        let result = write_socket_helper(Command::Prometheus, "ntp-test-stream-8").await?;
+
+        assert_eq!(
+            format!("{:?}", result.unwrap()),
+            format!("{:?}", ExitCode::SUCCESS)
+        );
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_control_socket_config() -> std::io::Result<()> {
+        let config: ObserveConfig = Default::default();
+
+        // be careful with copying: tests run concurrently and should use a unique socket name!
+        let path = std::env::temp_dir().join("ntp-test-stream-9");
+        if path.exists() {
+            std::fs::remove_file(&path).unwrap();
+        }
+
+        let peers_listener = create_unix_socket(&path)?;
+
+        let permissions: std::fs::Permissions = PermissionsExt::from_mode(config.mode);
+        std::fs::set_permissions(&path, permissions)?;
+
+        let update = ConfigUpdate {
+            log_filter: Some("foo".to_string()),
+            panic_threshold: Some(0.123),
+        };
+
+        let fut = super::run(Command::Config(update.clone()), path);
+        let handle = tokio::spawn(fut);
+
+        let (mut stream, _addr) = peers_listener.accept().await?;
+        let mut msg = Vec::with_capacity(16 * 1024);
+        let actual_update = read_json::<ConfigUpdate>(&mut stream, &mut msg).await?;
+
+        let result = handle.await.unwrap();
+
+        assert_eq!(
+            format!("{:?}", result.unwrap()),
+            format!("{:?}", ExitCode::SUCCESS)
+        );
+
+        assert_eq!(update, actual_update);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_control_socket_peer_invalid_input() -> std::io::Result<()> {
+        let config: ObserveConfig = Default::default();
+
+        // be careful with copying: tests run concurrently and should use a unique socket name!
+        let path = std::env::temp_dir().join("ntp-test-stream-10");
+        if path.exists() {
+            std::fs::remove_file(&path).unwrap();
+        }
+
+        let peers_listener = create_unix_socket(&path)?;
+
+        let permissions: std::fs::Permissions = PermissionsExt::from_mode(config.mode);
+        std::fs::set_permissions(&path, permissions)?;
+
+        let fut = super::run(Command::Peers, path);
+        let handle = tokio::spawn(fut);
+
+        let value = 42u32;
+
+        let (mut stream, _addr) = peers_listener.accept().await?;
+        write_json(&mut stream, &value).await?;
+
+        let result = handle.await.unwrap();
+
+        assert_eq!(
+            format!("{:?}", result.unwrap()),
+            format!("{:?}", ExitCode::FAILURE)
+        );
+
+        Ok(())
     }
 }
