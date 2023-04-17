8da6b5a3f22a4bcf691270eacd2fb8be3a09cc2d
Sander Maijers <3374183+sanmai-NL@users.noreply.github.com>
2023-04-17 20:44:56 +0200

    Differentiate `timex`: `gnu` & `musl` `target_env`

--- ntp-os-clock/src/unix.rs
+++ ntp-os-clock/src/unix.rs
@@ -15,7 +15,7 @@ use ntp_proto::{NtpClock, NtpDuration, NtpLeapIndicator, NtpTimestamp, PollInter
 
 // Libc has no good other way of obtaining this, so let's at least make our functions
 // more readable.
-#[cfg(target_os = "linux")]
+#[cfg(all(target_os = "linux", target_env = "gnu"))]
 pub(crate) const EMPTY_TIMEX: libc::timex = libc::timex {
     modes: 0,
     offset: 0,
@@ -51,6 +51,34 @@ pub(crate) const EMPTY_TIMEX: libc::timex = libc::timex {
     __unused9: 0,
     __unused10: 0,
     __unused11: 0,
+};
+
+#[cfg(all(target_os = "linux", target_env = "musl"))]
+pub(crate) const EMPTY_TIMEX: libc::timex = libc::timex {
+    modes: 0,
+    offset: 0,
+    freq: 0,
+    maxerror: 0,
+    esterror: 0,
+    status: 0,
+    constant: 0,
+    precision: 0,
+    tolerance: 0,
+    time: libc::timeval {
+        tv_sec: 0,
+        tv_usec: 0,
+    },
+    tick: 0,
+    ppsfreq: 0,
+    jitter: 0,
+    shift: 0,
+    stabil: 0,
+    jitcnt: 0,
+    calcnt: 0,
+    errcnt: 0,
+    stbcnt: 0,
+    tai: 0,
+    __padding: [0; 11]
 };
 
 #[cfg(any(target_os = "freebsd", target_os = "macos"))]
