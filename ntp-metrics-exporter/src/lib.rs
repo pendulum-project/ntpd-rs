fb31546a82a098acb35f763bb0b121f4219402da
Ruben Nijveld <ruben@tweedegolf.com>
2023-04-17 15:06:06 +0200

    Add unstable interface warnings to all internal crates

--- ntp-metrics-exporter/src/lib.rs
+++ ntp-metrics-exporter/src/lib.rs
@@ -1,3 +1,10 @@
+//! This crate contains the OpenMetrics/Prometheus metrics exporter for ntpd-rs, but
+//! is not intended as a public interface at this time. It follows the same version
+//! as the main ntpd-rs crate, but that version is not intended to give any
+//! stability guarantee. Use at your own risk.
+//!
+//! Please visit the [ntpd-rs](https://github.com/pendulum-project/ntpd-rs) project
+//! for more information.
 #![forbid(unsafe_code)]
 
 mod metrics;
--- ntp-udp/src/lib.rs
+++ ntp-udp/src/lib.rs
@@ -1,3 +1,10 @@
+//! This crate contains networking and timestamping code for ntpd-rs and is not
+//! intended as a public interface at this time. It follows the same version as the
+//! main ntpd-rs crate, but that version is not intended to give any stability
+//! guarantee. Use at your own risk.
+//!
+//! Please visit the [ntpd-rs](https://github.com/pendulum-project/ntpd-rs) project
+//! for more information.
 #![forbid(unsafe_op_in_unsafe_fn)]
 
 mod hwtimestamp;
