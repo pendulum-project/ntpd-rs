ce1dd41c2dada015cd079cff7e7ea2d1a1ef3b91
David Venhoek <david@tweedegolf.com>
2023-09-20 09:03:52 +0200

    Fix user creation scripts.

--- pkg/test-scripts/test-ntpd-rs.sh
+++ pkg/test-scripts/test-ntpd-rs.sh
@@ -5,6 +5,12 @@ set -x
 
 case $1 in
   post-install|post-upgrade)
+      # Ensure users are created
+      id ntpd-rs
+      id ntpd-rs-observe
+
+      # Ensure deamon and ctl client are present
+      # and configuration validates.
       echo -e "\nNTPD-RS HELP OUTPUT:"
       /usr/bin/ntp-daemon --help
       /usr/bin/ntp-ctl validate
