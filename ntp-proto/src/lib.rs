aa5fa411ada3de694b772b5568e27a19c4e95a65
Marlon Baeten <marlon@tweedegolf.com>
2023-10-17 14:40:22 +0200

    Fix fuzzer for versioned extension headers

--- ntp-proto/src/lib.rs
+++ ntp-proto/src/lib.rs
@@ -43,8 +43,8 @@ mod exports {
     #[cfg(feature = "__internal-fuzz")]
     pub use super::packet::ExtensionField;
     pub use super::packet::{
-        Cipher, CipherProvider, EncryptResult, NoCipher, NtpAssociationMode, NtpLeapIndicator,
-        NtpPacket, PacketParsingError,
+        Cipher, CipherProvider, EncryptResult, ExtensionHeaderVersion, NoCipher,
+        NtpAssociationMode, NtpLeapIndicator, NtpPacket, PacketParsingError,
     };
     #[cfg(feature = "__internal-fuzz")]
     pub use super::peer::fuzz_measurement_from_packet;
