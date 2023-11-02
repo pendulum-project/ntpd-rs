47140a954448c32486a9f434f6ff8aa88519941f
Tamme Dittrich <tamme@tweedegolf.com>
2023-11-02 12:02:55 +0100

    Implement extension fields for BloomFilter

--- ntp-proto/src/packet/v5/extension_fields.rs
+++ ntp-proto/src/packet/v5/extension_fields.rs
@@ -1,3 +1,7 @@
+use crate::packet::v5::server_reference_id::BloomFilter;
+use std::borrow::Cow;
+use std::io::Write;
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Type {
     DraftIdentification,
@@ -61,6 +65,111 @@ impl Type {
             Self::SecondaryReceiveTimestamp,
         ]
         .into_iter()
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct ReferenceIdRequest {
+    payload_len: u16,
+    offset: u16,
+}
+
+impl ReferenceIdRequest {
+    pub fn new(payload_len: u16, offset: u16) -> Option<Self> {
+        if payload_len % 4 != 0 {
+            return None;
+        }
+
+        if payload_len + offset > 512 {
+            return None;
+        }
+
+        Some(Self {
+            payload_len,
+            offset,
+        })
+    }
+
+    pub fn to_response<'filter>(
+        &self,
+        filter: &'filter BloomFilter,
+    ) -> Option<ReferenceIdResponse<'filter>> {
+        let offset = usize::from(self.offset);
+        let payload_len = usize::from(self.payload_len);
+
+        let bytes = filter
+            .as_bytes()
+            .as_slice()
+            .get(offset..)?
+            .get(..payload_len)?
+            .into();
+
+        Some(ReferenceIdResponse { bytes })
+    }
+
+    pub fn serialize(&self, mut writer: impl Write) -> std::io::Result<()> {
+        writer.write_all(&Type::ReferenceIdRequest.to_bits().to_be_bytes())?;
+        writer.write_all(&self.offset.to_be_bytes())?;
+        writer.write_all(&[0; 2])?;
+
+        let words = self.payload_len / 4;
+        assert_eq!(self.payload_len % 4, 0);
+        for _ in 1..words {
+            writer.write_all(&[0; 4])?;
+        }
+
+        Ok(())
+    }
+
+    pub(crate) fn offset(&self) -> u16 {
+        self.offset
+    }
+
+    pub(crate) fn payload_len(&self) -> u16 {
+        self.payload_len
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct ReferenceIdResponse<'a> {
+    bytes: Cow<'a, [u8]>,
+}
+
+impl<'a> ReferenceIdResponse<'a> {
+    pub fn new(bytes: &'a [u8]) -> Option<Self> {
+        if bytes.len() % 4 != 0 {
+            return None;
+        }
+
+        if bytes.len() > 512 {
+            return None;
+        }
+
+        Some(Self {
+            bytes: Cow::Borrowed(bytes),
+        })
+    }
+
+    pub fn into_owned(self) -> ReferenceIdResponse<'static> {
+        ReferenceIdResponse {
+            bytes: Cow::Owned(self.bytes.into_owned()),
+        }
+    }
+
+    pub fn serialize(&self, mut writer: impl Write) -> std::io::Result<()> {
+        let len: u16 = self.bytes.len().try_into().unwrap();
+        let len = len + 4; // Add room for type and length
+        assert_eq!(len % 4, 0);
+
+        writer.write_all(&Type::ReferenceIdResponse.to_bits().to_be_bytes())?;
+        writer.write_all(&len.to_be_bytes())?;
+        writer.write_all(self.bytes.as_ref())?;
+
+        Ok(())
+    }
+
+    pub fn bytes(&self) -> &[u8] {
+        &*self.bytes
     }
 }
 
