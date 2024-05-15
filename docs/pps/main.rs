1d6a71992ff201b0dbaec90d30b3d1a217daceb3
Arda Cengaver <a.cengaver@student.tudelft.nl>
2024-05-15 17:50:53 +0200

    convert the collected pps data (unix_timestamp) to UTC_timetamp

--- docs/pps/main.rs
+++ docs/pps/main.rs
@@ -1,14 +1,30 @@
 use std::os::unix::io::{AsRawFd, RawFd};
-use std::io::Error;
+use std::io::{Error, Result};
+use std::fs::File;
+use std::mem::MaybeUninit;
+use libc::{self, timespec, clock_gettime, CLOCK_REALTIME};
+use chrono::{NaiveDateTime, NaiveTimeZone, Utc};
 
 fn get_pps_time(fd: RawFd) -> Result<(), Error> {
-    let mut ts = timespec { tv_sec: 0, tv_nsec: 0 };
+    let mut ts = MaybeUninit::<timespec>::uninit();
+
     unsafe {
-        if clock_gettime(CLOCK_REALTIME, &mut ts) != 0 {
+        if clock_gettime(CLOCK_REALTIME, ts.as_mut_ptr()) != 0 {
             return Err(Error::last_os_error());
         }
     }
-    println!("Current time: {}.{}", ts.tv_sec, ts.tv_nsec);
+
+    let ts = unsafe { ts.assume_init() };
+    let timestamp = ts.tv_sec as f64 + ts.tv_nsec as f64 / 1_000_000_000.0;
+
+    // Convert Unix timestamp to NaiveDateTime
+    let naive_datetime = NaiveDateTime::from_timestamp(timestamp as i64, (timestamp.fract() * 1_000_000_000.0) as u32);
+
+    // Convert NaiveDateTime to Utc
+    let utc_datetime = NaiveTimeZone::from_utc_datetime(&Utc, &naive_datetime);
+
+    println!("Current UTC time: {}", utc_datetime);
+
     Ok(())
 }
 
