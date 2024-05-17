23b5bd41471857f0b985ce2a2733b6cd9c66790d
michielbakker <m.s.bakker-1@student.tudelft.nl>
2024-05-17 13:23:09 +0200

    Added tests for the NtpTimestamp part

--- docs/pps/main.rs
+++ docs/pps/main.rs
@@ -332,5 +332,58 @@ mod tests {
         let duration2 = NtpDuration { duration: 2 };
         assert_eq!((duration1 - duration2).duration, 6);
     }
+
+    
+     //NtpTimestamp Tests: 
+    #[test]
+    fn test_ntp_timestamp_from_bits() {
+        let bits = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00];
+        let timestamp = NtpTimestamp::from_bits(bits);
+        assert_eq!(timestamp.timestamp, 256);
+    }
+
+    #[test]
+    fn test_ntp_timestamp_to_bits() {
+        let timestamp = NtpTimestamp { timestamp: 256 };
+        assert_eq!(timestamp.to_bits(), [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00]);
+    }
+
+    #[test]
+    fn test_ntp_timestamp_from_seconds_nanos_since_ntp_era() {
+        let timestamp = NtpTimestamp::from_seconds_nanos_since_ntp_era(1, 0);
+        assert_eq!(timestamp.timestamp, 1u64 << 32);
+
+        let timestamp = NtpTimestamp::from_seconds_nanos_since_ntp_era(1, 500000000);
+        assert_eq!(timestamp.timestamp, (1u64 << 32) + (1u64 << 31));
+
+        let timestamp = NtpTimestamp::from_seconds_nanos_since_ntp_era(0, 500000000);
+        assert_eq!(timestamp.timestamp, 1u64 << 31);
+
+        let timestamp = NtpTimestamp::from_seconds_nanos_since_ntp_era(0, 0);
+        assert_eq!(timestamp.timestamp, 0);
+    }
+
+    #[test]
+    fn test_ntp_timestamp_from_unix_timestamp() {
+        let timestamp = NtpTimestamp::from_unix_timestamp(1620000000, 0);
+        assert_eq!(timestamp.timestamp, 3828988800u64 << 32);
+
+        let timestamp = NtpTimestamp::from_unix_timestamp(1620000000, 500000000);
+        assert_eq!(timestamp.timestamp, (3828988800u64 << 32) + (1u64 << 31));
+
+        let timestamp = NtpTimestamp::from_unix_timestamp(0, 500000000);
+        assert_eq!(timestamp.timestamp, (2208988800u64 << 32) + (1u64 << 31));
+
+        let timestamp = NtpTimestamp::from_unix_timestamp(0, 0);
+        assert_eq!(timestamp.timestamp, 2208988800u64 << 32);
+    }
+
+    #[test]
+    fn test_ntp_timestamp_sub() {
+        let timestamp1 = NtpTimestamp { timestamp: 9 };
+        let timestamp2 = NtpTimestamp { timestamp: 7 };
+        let duration = timestamp1 - timestamp2;
+        assert_eq!(duration.duration, 2);
+    }
 }
 
