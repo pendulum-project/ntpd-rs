fb8ff89c1a67f369794e64bd5ada8adf75177165
anikoloudis <a.a.nikoloudis@student.tudelft.nl>
2024-06-16 18:57:29 +0200

    add pps_source for the ntp proto

---
+++ ntp-proto/src/pps_source.rs
@@ -0,0 +1,106 @@
+use crate::time_types::{NtpInstant, NtpTimestamp};
+use crate::source::Measurement;
+use std::time::Duration;
+
+use tracing::{instrument, warn};
+
+#[derive(Debug)]
+pub struct PpsSource {
+
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct PpsSourceUpdate {
+    pub(crate) measurement: Option<Measurement>,
+}
+
+#[cfg(feature = "__internal-test")]
+impl PpsSourceUpdate {
+    pub fn measurement(measurement: Measurement) -> Self {
+        PpsSourceUpdate {
+            measurement: Some(measurement),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+#[allow(clippy::large_enum_variant)]
+pub enum PpsSourceAction {
+    /// Send a message over the network. When this is issued, the network port maybe changed.
+    Send(),
+    /// Send an update to [`System`](crate::system::System)
+    UpdateSystem(PpsSourceUpdate),
+    /// Call [`NtpSource::handle_timer`] after given duration
+    SetTimer(Duration),
+    /// A complete reset of the connection is necessary, including a potential new NTSKE client session and/or DNS lookup.
+    Reset,
+    /// We must stop talking to this particular server.
+    Demobilize,
+}
+
+#[derive(Debug)]
+pub struct PpsSourceActionIterator {
+    iter: <Vec<PpsSourceAction> as IntoIterator>::IntoIter,
+}
+
+impl Default for PpsSourceActionIterator {
+    fn default() -> Self {
+        Self {
+            iter: vec![].into_iter(),
+        }
+    }
+}
+
+impl Iterator for PpsSourceActionIterator {
+    type Item = PpsSourceAction;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
+    }
+}
+
+impl PpsSourceActionIterator {
+    fn from(data: Vec<PpsSourceAction>) -> Self {
+        Self {
+            iter: data.into_iter(),
+        }
+    }
+}
+
+macro_rules! actions {
+    [$($action:expr),*] => {
+        {
+            PpsSourceActionIterator::from(vec![$($action),*])
+        }
+    }
+}
+
+impl PpsSource {
+    #[instrument]
+    pub fn new() -> (Self, PpsSourceActionIterator) {
+        (
+            Self {},
+            actions!(PpsSourceAction::SetTimer(Duration::from_secs(0))),
+        )
+    }
+
+    #[instrument(skip(self))]
+    pub fn handle_incoming(
+        &mut self,
+        local_clock_time: NtpInstant,
+        send_time: NtpTimestamp,
+        recv_time: NtpTimestamp,
+    ) -> PpsSourceActionIterator {
+        
+        // generate a measurement
+        let measurement = Measurement::from_pps(
+            send_time,
+            recv_time,
+            local_clock_time,
+        );
+       
+        actions!(PpsSourceAction::UpdateSystem(PpsSourceUpdate {
+            measurement: Some(measurement),
+        }))
+    }
+}
