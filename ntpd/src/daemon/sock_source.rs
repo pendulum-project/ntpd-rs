fa3c752040833daf82f571eb779af90108fe9433
Michiel <michielp1807@gmail.com>
2024-10-03 10:40:50 +0200

    Forward SystemUpdates to controller for sock source

--- ntpd/src/daemon/sock_source.rs
+++ ntpd/src/daemon/sock_source.rs
@@ -1,8 +1,8 @@
-use std::{array::TryFromSliceError, path::Path};
+use std::{fmt::Display, path::Path};
 
 use ntp_proto::{
     Measurement, NtpClock, NtpDuration, NtpInstant, NtpLeapIndicator, ReferenceId,
-    SockSourceSnapshot, SockSourceUpdate, SourceController,
+    SockSourceSnapshot, SockSourceUpdate, SourceController, SystemSourceUpdate,
 };
 use tracing::debug;
 #[cfg(target_os = "linux")]
@@ -14,6 +14,7 @@ use crate::daemon::{exitcode, ntp_source::MsgForSystem};
 
 use super::{ntp_source::SourceChannels, spawn::SourceId};
 
+// Based on https://gitlab.com/gpsd/gpsd/-/blob/master/gpsd/timehint.c#L268
 #[derive(Debug)]
 struct SockSample {
     // tv_sec: i64,
@@ -24,19 +25,58 @@ struct SockSample {
     magic: i32,
 }
 
-// Based on https://gitlab.com/gpsd/gpsd/-/blob/master/gpsd/timehint.c#L268
 const SOCK_MAGIC: i32 = 0x534f434b;
 const SOCK_SAMPLE_SIZE: usize = 40;
-fn deserialize_sample(buf: [u8; SOCK_SAMPLE_SIZE]) -> Result<SockSample, TryFromSliceError> {
-    Ok(SockSample {
+
+#[derive(Debug)]
+enum SampleError {
+    IOError(std::io::Error),
+    SliceError(std::array::TryFromSliceError),
+    WrongSize(usize),
+    WrongMagic(i32),
+    WrongPulse(i32),
+}
+
+impl Display for SampleError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SampleError::IOError(e) => f.write_str(&e.to_string()),
+            SampleError::SliceError(e) => f.write_str(&e.to_string()),
+            SampleError::WrongSize(s) => f.write_fmt(format_args!("Invalid size {s}")),
+            SampleError::WrongMagic(m) => f.write_fmt(format_args!("Invalid magic {m}")),
+            SampleError::WrongPulse(p) => f.write_fmt(format_args!("Invalid pulse {p}")),
+        }
+    }
+}
+
+fn deserialize_sample(
+    result: Result<usize, std::io::Error>,
+    buf: [u8; SOCK_SAMPLE_SIZE],
+) -> Result<SockSample, SampleError> {
+    let size = result.map_err(SampleError::IOError)?;
+    if size != SOCK_SAMPLE_SIZE {
+        return Err(SampleError::WrongSize(size));
+    }
+
+    let sample = SockSample {
         // tv_sec: i64::from_le_bytes(buf[0..8].try_into()?),
         // tv_usec: i64::from_le_bytes(buf[8..16].try_into()?),
-        offset: f64::from_le_bytes(buf[16..24].try_into()?),
-        pulse: i32::from_le_bytes(buf[24..28].try_into()?),
-        leap: i32::from_le_bytes(buf[28..32].try_into()?),
+        offset: f64::from_le_bytes(buf[16..24].try_into().map_err(SampleError::SliceError)?),
+        pulse: i32::from_le_bytes(buf[24..28].try_into().map_err(SampleError::SliceError)?),
+        leap: i32::from_le_bytes(buf[28..32].try_into().map_err(SampleError::SliceError)?),
         // skip padding (4 bytes)
-        magic: i32::from_le_bytes(buf[36..40].try_into()?),
-    })
+        magic: i32::from_le_bytes(buf[36..40].try_into().map_err(SampleError::SliceError)?),
+    };
+
+    if sample.magic != SOCK_MAGIC {
+        return Err(SampleError::WrongMagic(sample.magic));
+    }
+
+    if sample.pulse != 0 {
+        return Err(SampleError::WrongPulse(sample.pulse));
+    }
+
+    Ok(sample)
 }
 
 pub(crate) struct SockSourceTask<C: 'static + NtpClock + Send, Controller: SourceController> {
@@ -64,75 +104,88 @@ where
 {
     async fn run(&mut self) {
         loop {
-            let mut data = [0; SOCK_SAMPLE_SIZE];
-            match self.socket.recv(&mut data).await {
-                Ok(_) => (),
-                Err(e) => {
-                    error!("Error receiving data from socket: {:?}", e);
-                    continue;
-                }
-            }
-            let sample = match deserialize_sample(data) {
-                Ok(s) => s,
-                Err(e) => {
-                    error!("Error deserializing sample: {:?}", e);
-                    continue;
-                }
-            };
-            debug!("received {:?}", sample);
+            let mut buf = [0; SOCK_SAMPLE_SIZE];
 
-            if sample.magic != SOCK_MAGIC {
-                error!("Incorrect sock magic: {}", sample.magic);
-                continue;
+            enum SelectResult<Controller: SourceController> {
+                SockRecv(Result<usize, std::io::Error>),
+                SystemUpdate(
+                    Result<
+                        SystemSourceUpdate<Controller::ControllerMessage>,
+                        tokio::sync::broadcast::error::RecvError,
+                    >,
+                ),
             }
 
-            if sample.pulse != 0 {
-                error!("Socket sample indicates PPS: {}", sample.pulse);
-                continue;
-            }
-
-            let leap = match sample.leap {
-                0 => NtpLeapIndicator::NoWarning,
-                1 => NtpLeapIndicator::Leap61,
-                2 => NtpLeapIndicator::Leap59,
-                _ => NtpLeapIndicator::Unknown,
-            };
-
-            let time = match self.clock.now() {
-                Ok(time) => time,
-                Err(e) => {
-                    error!(error = ?e, "There was an error retrieving the current time");
-                    std::process::exit(exitcode::NOPERM);
-                }
-            };
-
-            let measurement = Measurement {
-                delay: (),
-                offset: NtpDuration::from_seconds(sample.offset),
-                localtime: time,
-                monotime: NtpInstant::now(),
-
-                stratum: 0,
-                root_delay: NtpDuration::ZERO,
-                root_dispersion: NtpDuration::ZERO,
-                leap,
-                precision: 0, // TODO: compute on startup?
-            };
-
-            let controller_message = self.controller.handle_measurement(measurement);
-
-            let update = SockSourceUpdate {
-                snapshot: SockSourceSnapshot {
-                    source_id: ReferenceId::SOCK,
-                    stratum: 0,
+            let selected: SelectResult<Controller> = tokio::select! {
+                result = self.socket.recv(&mut buf) => {
+                    SelectResult::SockRecv(result)
                 },
-                message: controller_message,
+                result = self.channels.system_update_receiver.recv() => {
+                    SelectResult::SystemUpdate(result)
+                }
             };
-            self.channels
-                .msg_for_system_sender
-                .send(MsgForSystem::SockSourceUpdate(self.index, update))
-                .await
-                .ok();
+
+            match selected {
+                SelectResult::SockRecv(result) => match deserialize_sample(result, buf) {
+                    Ok(sample) => {
+                        debug!("received {:?}", sample);
+                        let leap = match sample.leap {
+                            0 => NtpLeapIndicator::NoWarning,
+                            1 => NtpLeapIndicator::Leap61,
+                            2 => NtpLeapIndicator::Leap59,
+                            _ => NtpLeapIndicator::Unknown,
+                        };
+
+                        let time = match self.clock.now() {
+                            Ok(time) => time,
+                            Err(e) => {
+                                error!(error = ?e, "There was an error retrieving the current time");
+                                std::process::exit(exitcode::NOPERM);
+                            }
+                        };
+
+                        let measurement = Measurement {
+                            delay: (),
+                            offset: NtpDuration::from_seconds(sample.offset),
+                            localtime: time,
+                            monotime: NtpInstant::now(),
+
+                            stratum: 0,
+                            root_delay: NtpDuration::ZERO,
+                            root_dispersion: NtpDuration::ZERO,
+                            leap,
+                            precision: 0, // TODO: compute on startup?
+                        };
+
+                        let controller_message = self.controller.handle_measurement(measurement);
+
+                        let update = SockSourceUpdate {
+                            snapshot: SockSourceSnapshot {
+                                source_id: ReferenceId::SOCK,
+                                stratum: 0,
+                            },
+                            message: controller_message,
+                        };
+                        self.channels
+                            .msg_for_system_sender
+                            .send(MsgForSystem::SockSourceUpdate(self.index, update))
+                            .await
+                            .ok();
+                    }
+                    Err(e) => {
+                        error!("Error deserializing sample: {}", e);
+                        continue;
+                    }
+                },
+                SelectResult::SystemUpdate(result) => match result {
+                    Ok(update) => {
+                        self.controller.handle_message(update.message);
+                    }
+                    Err(e) => {
+                        error!("Error receiving system update: {:?}", e)
+                    }
+                },
+            };
         }
     }
 
