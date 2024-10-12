9e056d469c75fdc035b8ceb73ec46c4863f46f04
van-sprundel <ramonvansprundel@gmail.com>
2024-10-12 19:48:17 +0200

    Remove unused `Result<_>` in function

--- ntpd/src/daemon/system.rs
+++ ntpd/src/daemon/system.rs
@@ -64,7 +64,7 @@ impl<T: Wait> Future for SingleshotSleep<T> {
                 this.enabled = false;
                 std::task::Poll::Ready(v)
             }
-            u => u,
+            u @ std::task::Poll::Pending => u,
         }
     }
 }
@@ -103,7 +103,7 @@ pub async fn spawn<Controller: TimeSyncController<Clock = NtpClockWrapper, Sourc
         synchronization_config,
         algorithm_config,
         source_defaults_config,
-        keyset,
+        &keyset,
         ip_list,
         !source_configs.is_empty(),
     );
@@ -111,43 +111,23 @@ pub async fn spawn<Controller: TimeSyncController<Clock = NtpClockWrapper, Sourc
     for source_config in source_configs {
         match source_config {
             NtpSourceConfig::Standard(cfg) => {
-                system
-                    .add_spawner(StandardSpawner::new(cfg.clone()))
-                    .map_err(|e| {
-                        tracing::error!("Could not spawn source: {}", e);
-                        std::io::Error::new(std::io::ErrorKind::Other, e)
-                    })?;
+                system.add_spawner(StandardSpawner::new(cfg.clone()));
             }
             NtpSourceConfig::Nts(cfg) => {
-                system
-                    .add_spawner(NtsSpawner::new(cfg.clone()))
-                    .map_err(|e| {
-                        tracing::error!("Could not spawn source: {}", e);
-                        std::io::Error::new(std::io::ErrorKind::Other, e)
-                    })?;
+                system.add_spawner(NtsSpawner::new(cfg.clone()));
             }
             NtpSourceConfig::Pool(cfg) => {
-                system
-                    .add_spawner(PoolSpawner::new(cfg.clone()))
-                    .map_err(|e| {
-                        tracing::error!("Could not spawn source: {}", e);
-                        std::io::Error::new(std::io::ErrorKind::Other, e)
-                    })?;
+                system.add_spawner(PoolSpawner::new(cfg.clone()));
             }
             #[cfg(feature = "unstable_nts-pool")]
             NtpSourceConfig::NtsPool(cfg) => {
-                system
-                    .add_spawner(NtsPoolSpawner::new(cfg.clone()))
-                    .map_err(|e| {
-                        tracing::error!("Could not spawn source: {}", e);
-                        std::io::Error::new(std::io::ErrorKind::Other, e)
-                    })?;
+                system.add_spawner(NtsPoolSpawner::new(cfg.clone()));
             }
         }
     }
 
     for server_config in server_configs {
-        system.add_server(server_config.to_owned()).await;
+        system.add_server(server_config.to_owned());
     }
 
     let handle = tokio::spawn(async move {
@@ -214,7 +194,7 @@ impl<
         synchronization_config: SynchronizationConfig,
         algorithm_config: Controller::AlgorithmConfig,
         source_defaults_config: SourceDefaultsConfig,
-        keyset: tokio::sync::watch::Receiver<Arc<KeySet>>,
+        keyset: &tokio::sync::watch::Receiver<Arc<KeySet>>,
         ip_list: tokio::sync::watch::Receiver<Arc<[IpAddr]>>,
         have_sources: bool,
     ) -> (Self, DaemonChannels) {
@@ -279,10 +259,7 @@ impl<
         )
     }
 
-    fn add_spawner(
-        &mut self,
-        spawner: impl Spawner + Send + Sync + 'static,
-    ) -> Result<SpawnerId, C::Error> {
+    fn add_spawner(&mut self, spawner: impl Spawner + Send + Sync + 'static) -> SpawnerId {
         let (notify_tx, notify_rx) = mpsc::channel(MESSAGE_BUFFER_SIZE);
         let id = spawner.get_id();
         let spawner_data = SystemSpawnerData { id, notify_tx };
@@ -291,7 +268,7 @@ impl<
         let spawn_tx = self.spawn_tx.clone();
         // tokio::spawn(async move { spawner.run(spawn_tx, notify_rx).await });
         tokio::spawn(spawner_task(spawner, spawn_tx, notify_rx));
-        Ok(id)
+        id
     }
 
     async fn run(&mut self, mut wait: Pin<&mut SingleshotSleep<T>>) -> std::io::Result<()> {
@@ -466,7 +443,10 @@ impl<
         info!(source_id=?source_id, addr=?params.addr, spawner=?spawner_id, "new source");
         self.sources.insert(
             source_id,
-            SourceState { spawner_id, source_id },
+            SourceState {
+                spawner_id,
+                source_id,
+            },
         );
 
         let (source, initial_actions) = if let Some(nts) = params.nts.take() {
@@ -515,7 +495,7 @@ impl<
         Ok(())
     }
 
-    async fn add_server(&mut self, config: ServerConfig) {
+    fn add_server(&mut self, config: ServerConfig) {
         let stats = ServerStats::default();
         self.servers.push(ServerData {
             stats: stats.clone(),
