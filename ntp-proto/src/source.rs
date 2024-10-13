f0f4dde3aea1c22682a37b1dc579ee3d35179b94
van-sprundel <ramonvansprundel@gmail.com>
2024-10-13 11:31:44 +0200

    Mark debug impl with finish_non_exhaustive for hidden fields

--- ntp-proto/src/source.rs
+++ ntp-proto/src/source.rs
@@ -50,7 +50,7 @@ impl std::fmt::Debug for SourceNtsData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("SourceNtsData")
             .field("cookies", &self.cookies)
-            .finish()
+            .finish_non_exhaustive()
     }
 }
 
