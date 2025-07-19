61a1580689ca1f0ef0f39c48a7401bf5d31dcb09
David Venhoek <david@tweedegolf.com>
2025-07-19 13:50:21 +0200

    Added packet generation.

--- ntp-proto/src/csptp/mod.rs
+++ ntp-proto/src/csptp/mod.rs
@@ -1,6 +1,6 @@
 use std::io::Cursor;
 
-use statime::datastructures::WireFormat;
+use statime::datastructures::{common::TlvSetBuilder, WireFormat};
 
 const CSPTP_REQUEST_TLV: u16 = 0xFF00;
 const CSPTP_RESPONSE_TLV: u16 = 0xFF01;
@@ -63,13 +63,103 @@ impl<'a> CsptpPacket<'a> {
 
     pub fn get_origin_timestamp(&self) -> Option<statime::datastructures::common::WireTimestamp> {
         match self.inner.body {
-            statime::datastructures::messages::MessageBody::Sync(sync_message) => Some(sync_message.origin_timestamp),
-            _ => None
+            statime::datastructures::messages::MessageBody::Sync(sync_message) => {
+                Some(sync_message.origin_timestamp)
+            }
+            _ => None,
         }
     }
 
     pub fn get_correction(&self) -> statime::datastructures::common::TimeInterval {
         self.inner.header.correction_field
+    }
+
+    pub fn request(buffer: &'a mut [u8], sequence_id: u16) -> Self {
+        let flags = [0u8; 4];
+        let request_tlv = statime::datastructures::common::Tlv {
+            tlv_type: statime::datastructures::common::TlvType::Reserved(CSPTP_REQUEST_TLV),
+            value: flags.as_slice().into(),
+        };
+        let mut tlvs = statime::datastructures::common::TlvSetBuilder::new(buffer);
+        tlvs.add(request_tlv).unwrap();
+        Self {
+            inner: statime::datastructures::messages::Message {
+                header: statime::datastructures::messages::Header {
+                    sdo_id: statime::config::SdoId::try_from(0x300).unwrap(),
+                    version: statime::datastructures::messages::PtpVersion::new(2, 0).unwrap(),
+                    domain_number: 0,
+                    alternate_master_flag: false,
+                    two_step_flag: false,
+                    unicast_flag: true,
+                    ptp_profile_specific_1: false,
+                    ptp_profile_specific_2: false,
+                    leap61: false,
+                    leap59: false,
+                    current_utc_offset_valid: false,
+                    ptp_timescale: true,
+                    time_tracable: false,
+                    frequency_tracable: false,
+                    synchronization_uncertain: false,
+                    correction_field: Default::default(),
+                    source_port_identity: Default::default(),
+                    sequence_id,
+                    log_message_interval: 0x7f,
+                },
+                body: statime::datastructures::messages::MessageBody::Sync(
+                    statime::datastructures::messages::sync::SyncMessage {
+                        origin_timestamp: Default::default(),
+                    },
+                ),
+                suffix: tlvs.build(),
+            },
+        }
+    }
+
+    pub fn timestamp_response(
+        buffer: &'a mut [u8],
+        request: CsptpPacket<'_>,
+        receive_timestamp: statime::datastructures::common::WireTimestamp,
+        send_timestamp: statime::datastructures::common::WireTimestamp,
+    ) -> Self {
+        let mut tlvs = TlvSetBuilder::new(buffer);
+        let mut innerbuf = [0u8;18];
+        receive_timestamp.serialize(&mut innerbuf[0..10]).unwrap();
+        request.get_correction().serialize(&mut innerbuf[10..18]).unwrap();
+        tlvs.add(statime::datastructures::common::Tlv {
+            tlv_type: statime::datastructures::common::TlvType::Reserved(CSPTP_RESPONSE_TLV),
+            value: innerbuf.as_slice().into(),
+        }).unwrap();
+        Self {
+            inner: statime::datastructures::messages::Message {
+                header: statime::datastructures::messages::Header {
+                    sdo_id: statime::config::SdoId::try_from(0x300).unwrap(),
+                    version: statime::datastructures::messages::PtpVersion::new(2, 0).unwrap(),
+                    domain_number: 0,
+                    alternate_master_flag: false,
+                    two_step_flag: false,
+                    unicast_flag: true,
+                    ptp_profile_specific_1: false,
+                    ptp_profile_specific_2: false,
+                    leap61: false,
+                    leap59: false,
+                    current_utc_offset_valid: false,
+                    ptp_timescale: true,
+                    time_tracable: false,
+                    frequency_tracable: false,
+                    synchronization_uncertain: false,
+                    correction_field: Default::default(),
+                    source_port_identity: Default::default(),
+                    sequence_id: request.inner.header.sequence_id,
+                    log_message_interval: 0x7f,
+                },
+                body: statime::datastructures::messages::MessageBody::Sync(
+                    statime::datastructures::messages::sync::SyncMessage {
+                        origin_timestamp: send_timestamp,
+                    },
+                ),
+                suffix: tlvs.build(),
+            },
+        }
     }
 }
 
