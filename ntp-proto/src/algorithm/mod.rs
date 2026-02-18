f07e206a498c8d2149d7a834fc16fc0789de01ba
David Venhoek <david@tweedegolf.com>
2026-02-18 13:51:16 +0100

    Split algorithm traits and measurement struct into internal and external.
    This will enable us to more easily change the external ones.

--- ntp-proto/src/algorithm/mod.rs
+++ ntp-proto/src/algorithm/mod.rs
@@ -48,6 +48,148 @@ impl<ControllerMessage> Default for StateUpdate<ControllerMessage> {
     }
 }
 
+pub trait InternalTimeSyncController: Sized + Send + 'static {
+    type Clock: NtpClock;
+    type AlgorithmConfig: Debug + Copy + DeserializeOwned + Send;
+    type ControllerMessage: Debug + Clone + Send + 'static;
+    type SourceMessage: Debug + Clone + Send + 'static;
+    type NtpSourceController: InternalSourceController<
+            ControllerMessage = Self::ControllerMessage,
+            SourceMessage = Self::SourceMessage,
+            MeasurementDelay = NtpDuration,
+        >;
+    type OneWaySourceController: InternalSourceController<
+            ControllerMessage = Self::ControllerMessage,
+            SourceMessage = Self::SourceMessage,
+            MeasurementDelay = (),
+        >;
+
+    /// Create a new clock controller controlling the given clock
+    fn new(
+        clock: Self::Clock,
+        synchronization_config: SynchronizationConfig,
+        algorithm_config: Self::AlgorithmConfig,
+    ) -> Result<Self, <Self::Clock as NtpClock>::Error>;
+
+    /// Take control of the clock (should not be done in new!)
+    fn take_control(&mut self) -> Result<(), <Self::Clock as NtpClock>::Error>;
+
+    /// Create a new source with given identity
+    fn add_source(&mut self, id: ClockId, source_config: SourceConfig)
+    -> Self::NtpSourceController;
+    /// Create a new one way source with given identity (used e.g. with GPS sock sources)
+    fn add_one_way_source(
+        &mut self,
+        id: ClockId,
+        source_config: SourceConfig,
+        measurement_noise_estimate: f64,
+        period: Option<f64>,
+    ) -> Self::OneWaySourceController;
+    /// Notify the controller that a previous source has gone
+    fn remove_source(&mut self, id: ClockId);
+    /// Notify the controller that the status of a source (whether
+    /// or not it is usable for synchronization) has changed.
+    fn source_update(&mut self, id: ClockId, usable: bool);
+    /// Notify the controller of a new measurement from a source.
+    /// The list of SourceIds is used for loop detection, with the
+    /// first SourceId given considered the primary source used.
+    fn source_message(
+        &mut self,
+        id: ClockId,
+        message: Self::SourceMessage,
+    ) -> StateUpdate<Self::ControllerMessage>;
+    /// Non-message driven update (queued via next_update)
+    fn time_update(&mut self) -> StateUpdate<Self::ControllerMessage>;
+}
+
+pub trait InternalSourceController: Sized + Send + 'static {
+    type ControllerMessage: Debug + Clone + Send + 'static;
+    type SourceMessage: Debug + Clone + Send + 'static;
+    type MeasurementDelay: Debug + Copy + Clone;
+
+    fn handle_message(&mut self, message: Self::ControllerMessage);
+
+    fn handle_measurement(
+        &mut self,
+        measurement: InternalMeasurement<Self::MeasurementDelay>,
+    ) -> Option<Self::SourceMessage>;
+
+    fn desired_poll_interval(&self) -> PollInterval;
+
+    fn observe(&self) -> ObservableSourceTimedata;
+}
+
+mod kalman;
+
+pub use kalman::{
+    KalmanClockController, KalmanControllerMessage, KalmanSourceController, KalmanSourceMessage,
+    TwoWayKalmanSourceController, config::AlgorithmConfig,
+};
+
+#[derive(Debug, Copy, Clone)]
+pub struct InternalMeasurement<D: Debug + Copy + Clone> {
+    pub delay: D,
+    pub offset: NtpDuration,
+    pub localtime: NtpTimestamp,
+
+    pub stratum: u8,
+    pub root_delay: NtpDuration,
+    pub root_dispersion: NtpDuration,
+    pub leap: NtpLeapIndicator,
+    pub precision: i8,
+}
+
+impl<D: Debug + Copy + Clone> From<Measurement<D>> for InternalMeasurement<D> {
+    fn from(value: Measurement<D>) -> Self {
+        Self {
+            delay: value.delay,
+            offset: value.offset,
+            localtime: value.localtime,
+            stratum: value.stratum,
+            root_delay: value.root_delay,
+            root_dispersion: value.root_dispersion,
+            leap: value.leap,
+            precision: value.precision,
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Measurement<D: Debug + Copy + Clone> {
+    pub delay: D,
+    pub offset: NtpDuration,
+    pub localtime: NtpTimestamp,
+
+    pub stratum: u8,
+    pub root_delay: NtpDuration,
+    pub root_dispersion: NtpDuration,
+    pub leap: NtpLeapIndicator,
+    pub precision: i8,
+}
+
+impl Measurement<NtpDuration> {
+    pub(crate) fn from_packet(
+        packet: &NtpPacket,
+        send_timestamp: NtpTimestamp,
+        recv_timestamp: NtpTimestamp,
+    ) -> Self {
+        Self {
+            delay: (recv_timestamp - send_timestamp)
+                - (packet.transmit_timestamp() - packet.receive_timestamp()),
+            offset: ((packet.receive_timestamp() - send_timestamp)
+                + (packet.transmit_timestamp() - recv_timestamp))
+                / 2,
+            localtime: send_timestamp + (recv_timestamp - send_timestamp) / 2,
+
+            stratum: packet.stratum(),
+            root_delay: packet.root_delay(),
+            root_dispersion: packet.root_dispersion(),
+            leap: packet.leap(),
+            precision: packet.precision(),
+        }
+    }
+}
+
 pub trait TimeSyncController: Sized + Send + 'static {
     type Clock: NtpClock;
     type AlgorithmConfig: Debug + Copy + DeserializeOwned + Send;
@@ -102,6 +244,65 @@ pub trait TimeSyncController: Sized + Send + 'static {
     fn time_update(&mut self) -> StateUpdate<Self::ControllerMessage>;
 }
 
+impl<T: InternalTimeSyncController> TimeSyncController for T {
+    type Clock = T::Clock;
+    type AlgorithmConfig = T::AlgorithmConfig;
+    type ControllerMessage = T::ControllerMessage;
+    type SourceMessage = T::SourceMessage;
+    type NtpSourceController = T::NtpSourceController;
+    type OneWaySourceController = T::OneWaySourceController;
+
+    fn new(
+        clock: Self::Clock,
+        synchronization_config: SynchronizationConfig,
+        algorithm_config: Self::AlgorithmConfig,
+    ) -> Result<Self, <Self::Clock as NtpClock>::Error> {
+        T::new(clock, synchronization_config, algorithm_config)
+    }
+
+    fn take_control(&mut self) -> Result<(), <Self::Clock as NtpClock>::Error> {
+        T::take_control(self)
+    }
+
+    fn add_source(
+        &mut self,
+        id: ClockId,
+        source_config: SourceConfig,
+    ) -> Self::NtpSourceController {
+        T::add_source(self, id, source_config)
+    }
+
+    fn add_one_way_source(
+        &mut self,
+        id: ClockId,
+        source_config: SourceConfig,
+        measurement_noise_estimate: f64,
+        period: Option<f64>,
+    ) -> Self::OneWaySourceController {
+        T::add_one_way_source(self, id, source_config, measurement_noise_estimate, period)
+    }
+
+    fn remove_source(&mut self, id: ClockId) {
+        T::remove_source(self, id);
+    }
+
+    fn source_update(&mut self, id: ClockId, usable: bool) {
+        T::source_update(self, id, usable);
+    }
+
+    fn source_message(
+        &mut self,
+        id: ClockId,
+        message: Self::SourceMessage,
+    ) -> StateUpdate<Self::ControllerMessage> {
+        T::source_message(self, id, message)
+    }
+
+    fn time_update(&mut self) -> StateUpdate<Self::ControllerMessage> {
+        T::time_update(self)
+    }
+}
+
 pub trait SourceController: Sized + Send + 'static {
     type ControllerMessage: Debug + Clone + Send + 'static;
     type SourceMessage: Debug + Clone + Send + 'static;
@@ -119,45 +320,27 @@ pub trait SourceController: Sized + Send + 'static {
     fn observe(&self) -> ObservableSourceTimedata;
 }
 
-mod kalman;
+impl<T: InternalSourceController> SourceController for T {
+    type ControllerMessage = T::ControllerMessage;
+    type SourceMessage = T::SourceMessage;
+    type MeasurementDelay = T::MeasurementDelay;
 
-pub use kalman::{
-    KalmanClockController, KalmanControllerMessage, KalmanSourceController, KalmanSourceMessage,
-    TwoWayKalmanSourceController, config::AlgorithmConfig,
-};
+    fn handle_message(&mut self, message: Self::ControllerMessage) {
+        T::handle_message(self, message);
+    }
 
-#[derive(Debug, Copy, Clone)]
-pub struct Measurement<D: Debug + Copy + Clone> {
-    pub delay: D,
-    pub offset: NtpDuration,
-    pub localtime: NtpTimestamp,
+    fn handle_measurement(
+        &mut self,
+        measurement: Measurement<Self::MeasurementDelay>,
+    ) -> Option<Self::SourceMessage> {
+        T::handle_measurement(self, measurement.into())
+    }
 
-    pub stratum: u8,
-    pub root_delay: NtpDuration,
-    pub root_dispersion: NtpDuration,
-    pub leap: NtpLeapIndicator,
-    pub precision: i8,
-}
+    fn desired_poll_interval(&self) -> PollInterval {
+        T::desired_poll_interval(self)
+    }
 
-impl Measurement<NtpDuration> {
-    pub(crate) fn from_packet(
-        packet: &NtpPacket,
-        send_timestamp: NtpTimestamp,
-        recv_timestamp: NtpTimestamp,
-    ) -> Self {
-        Self {
-            delay: (recv_timestamp - send_timestamp)
-                - (packet.transmit_timestamp() - packet.receive_timestamp()),
-            offset: ((packet.receive_timestamp() - send_timestamp)
-                + (packet.transmit_timestamp() - recv_timestamp))
-                / 2,
-            localtime: send_timestamp + (recv_timestamp - send_timestamp) / 2,
-
-            stratum: packet.stratum(),
-            root_delay: packet.root_delay(),
-            root_dispersion: packet.root_dispersion(),
-            leap: packet.leap(),
-            precision: packet.precision(),
-        }
+    fn observe(&self) -> ObservableSourceTimedata {
+        T::observe(self)
     }
 }
