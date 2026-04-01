46466dc551a4d00e6c0c202368388d3fc796b3fd
David Venhoek <david@tweedegolf.com>
2026-04-01 13:44:21 +0200

    Remove leap indicator from ptp-wire.
    It is not used for the on-the-wire communication types.

--- ptp-wire/src/common/mod.rs
+++ ptp-wire/src/common/mod.rs
@@ -3,7 +3,6 @@
 mod clock_accuracy;
 mod clock_identity;
 mod clock_quality;
-mod leap_indicator;
 mod port_identity;
 mod time_interval;
 mod time_source;
@@ -13,8 +12,7 @@ mod tlv;
 pub use clock_accuracy::*;
 pub use clock_identity::*;
 pub use clock_quality::*;
-pub use leap_indicator::*;
-pub(crate) use port_identity::*;
+pub use port_identity::*;
 pub use time_interval::*;
 pub use time_source::*;
 pub use timestamp::*;
