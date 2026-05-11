ebc816a6d5cdf074396c5de14566979b6a812c49
David Venhoek <david@tweedegolf.com>
2026-05-11 15:16:00 +0200

    Implemented CSPTP server functionality.

--- statime-csptp/src/lib.rs
+++ statime-csptp/src/lib.rs
@@ -138,16 +138,41 @@ use statime_wire::{ClockIdentity, ClockQuality};
 #[cfg(feature = "std")]
 extern crate std;
 
+mod manager;
 mod messages;
+mod platform;
+mod server;
 
-// TODO: Figure out how to actually deal with the state.
-struct CsptpState {
-    grandmaster_identity: ClockIdentity,
-    grandmaster_priority_1: u8,
-    grandmaster_priority_2: u8,
-    grandmaster_clock_quality: ClockQuality,
-    steps_removed: u16,
-    ptp_timescale: bool,
-    time_traceable: bool,
-    frequency_traceable: bool,
+pub use manager::{CsptpConfig, CsptpManager};
+pub use platform::{InternalState, StateMutex};
+pub use server::{ServerRecvResult, ServerSocket, serve};
+
+/// Observable CSPTP state
+#[derive(Debug, Copy, Clone)]
+pub struct CsptpState {
+    /// Clock identity of grandmaster currently in use. This will be the local
+    /// clock identity if there is no upstream time source or the if the
+    /// upstream time source does not use CSPTP
+    pub grandmaster_identity: ClockIdentity,
+    /// Priority 1 for the grandmaster currently in use. This will be the local
+    /// value if there is no upstream time source or the if the upstream time
+    /// source does not use CSPTP
+    pub grandmaster_priority_1: u8,
+    /// Priority 2 for the grandmaster currently in use. This will be the local
+    /// value if there is no upstream time source or the if the upstream time
+    /// source does not use CSPTP
+    pub grandmaster_priority_2: u8,
+    /// Clock quality for the grandmaster currently in use. This will be the
+    /// local value if there is no upstream time source or the if the upstream
+    /// time source does not use CSPTP
+    pub grandmaster_clock_quality: ClockQuality,
+    /// Steps removed from the current grandmaster. Will be 0 if there is no
+    /// upstream time source or the upstream time source does not use CSPTP.
+    pub steps_removed: u16,
+    /// Whether the ptp timescale is in use.
+    pub ptp_timescale: bool,
+    /// Whether the current time is traceable.
+    pub time_traceable: bool,
+    /// Whether the current frequency is traceable.
+    pub frequency_traceable: bool,
 }
