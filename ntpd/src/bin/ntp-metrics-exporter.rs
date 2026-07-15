f228cd0fbcef4d0f26a0d43cd963f2cc047a9b27
David Venhoek <david@tweedegolf.com>
2026-07-15 11:58:50 +0200

    Configure lints for the whole workspace in one go.

--- ntpd/src/bin/ntp-metrics-exporter.rs
+++ ntpd/src/bin/ntp-metrics-exporter.rs
@@ -1,4 +1,7 @@
 #![forbid(unsafe_code)]
+#![allow(missing_docs)]
+// FIXME: the lints below should be reenabled. Please fix them with a per-lint
+// PR fixing that one lint and enabling it accross all crates.
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     ntpd::metrics_exporter_main()
