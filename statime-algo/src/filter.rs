2d7f4de61e97f577cebad3e7aa0d1cb0433080d6
David Venhoek <david@tweedegolf.com>
2026-08-05 17:17:06 +0200

    Properly activate internal links.

--- statime-algo/src/filter.rs
+++ statime-algo/src/filter.rs
@@ -388,6 +388,15 @@ impl LinkFilter {
                 }
                 return Ok(self);
             }
+        } else if !link.active {
+            link.active = true;
+            if is_tracked_link {
+                self.estimation_state = self.estimation_state.add_link(
+                    direction.link_id(),
+                    (estimates.delay, estimates.noise).into(),
+                    decay_rate,
+                )?;
+            }
         }
 
         self.estimation_state = self.estimation_state.measurement(
